60e32610d598e2c313bd9558b070608c4693c94b
Bryan Hockey <bhock@umich.edu>
2018-06-13 10:41:18 -0500

    Manage haproxy
    See AEIM-1006
    See AEIM-1258

---
+++ manifests/profile/haproxy.pp
@@ -0,0 +1,55 @@
+# Copyright (c) 2018 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+
+# haproxy
+#
+# @example
+#   include nebula::profile::haproxy
+class nebula::profile::haproxy(String $floating_ip) {
+  service { 'haproxy':
+    ensure     => 'running',
+    enable     => true,
+    hasrestart => true,
+  }
+
+  package { 'haproxy': }
+  package { 'haproxyctl': }
+
+  file { '/etc/haproxy/haproxy.cfg':
+    ensure  => 'present',
+    mode    => '0644',
+    content => template('nebula/profile/haproxy/haproxy.cfg.erb'),
+    require => Package['haproxy'],
+    notify  => Service['haproxy'],
+  }
+
+  $nodes_for_role = nodes_for_role('nebula::role::webhost::www_lib')
+  $nodes_for_datacenter = nodes_for_datacenter($::datacenter)
+  $datacenter = $::datacenter
+
+  file { '/etc/haproxy/backends.cfg':
+    ensure  => 'present',
+    mode    => '0644',
+    content => template('nebula/profile/haproxy/backends.cfg.erb'),
+    require => Package['haproxy'],
+    notify  => Service['haproxy'],
+  }
+
+  file { '/etc/haproxy/frontends.cfg':
+    ensure  => 'present',
+    mode    => '0644',
+    content => template('nebula/profile/haproxy/frontends.cfg.erb'),
+    require => Package['haproxy'],
+    notify  => Service['haproxy'],
+  }
+
+  file { '/etc/default/haproxy':
+    ensure  => 'present',
+    mode    => '0644',
+    content => template('nebula/profile/haproxy/default.erb'),
+    require => Package['haproxy'],
+    notify  => Service['haproxy'],
+  }
+
+}
