aaaace8c801704083e5c8a4ea018cc85455b3572
Matthew Alexander LaChance <mattlach@umich.edu>
2018-06-27 08:35:00 -0400

    AEIM-1273 - Remove vmhost defaults parameter

--- manifests/profile/vmhost/host.pp
+++ manifests/profile/vmhost/host.pp
@@ -14,13 +14,33 @@
 # @example
 #   include nebula::profile::vmhost::host
 class nebula::profile::vmhost::host (
-  Hash $defaults,
-  Hash $vms,
+  Hash    $vms,
+  String  $build,
+  Integer $cpus,
+  Integer $disk,
+  Integer $ram,
+  String  $domain,
+  String  $filehost,
+  String  $image_dir,
+  String  $net_interface,
+  String  $netmask,
+  String  $gateway,
+  Array   $nameservers,
 ) {
   $vms.each |$vm_name, $vm_settings| {
     nebula::virtual_machine {
       default:
-        * => $defaults,
+        build         => $build,
+        cpus          => $cpus,
+        disk          => $disk,
+        ram           => $ram,
+        domain        => $domain,
+        filehost      => $filehost,
+        image_dir     => $image_dir,
+        net_interface => $net_interface,
+        netmask       => $netmask,
+        gateway       => $gateway,
+        nameservers   => $nameservers,
       ;
       $vm_name:
         * => $vm_settings,
