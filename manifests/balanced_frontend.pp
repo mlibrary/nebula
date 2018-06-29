6952e248469f40e507858c3d00625b367d1c8d2c
Matthew Alexander LaChance <mattlach@umich.edu>
2018-06-29 08:46:44 -0400

    Add balanced_frontend defined type

---
+++ manifests/balanced_frontend.pp
@@ -0,0 +1,11 @@
+# Copyright (c) 2018 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+
+# Load-balanced frontend
+#
+# @example
+#   nebula::balanced_frontend { 'namevar': }
+define nebula::balanced_frontend(
+) {
+}
