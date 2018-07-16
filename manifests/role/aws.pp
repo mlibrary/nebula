eeb018b0f6dbd444a789d2776591016cd54f90e5
Bryan Hockey <bhock@umich.edu>
2018-07-16 11:21:10 -0500

    Preserve debian-9-only functionality

--- manifests/role/aws.pp
+++ manifests/role/aws.pp
@@ -2,29 +2,24 @@
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
 
-# Minimal umich server
+# Minimal aws server
 #
 # @example
-#   include nebula::role::umich
-class nebula::role::umich (
-  $bridge_network = false,
-) {
+#   include nebula::role::aws
+class nebula::role::aws {
 
   include nebula::profile::base
   if $facts['os']['release']['major'] == '9' {
-    include nebula::profile::afs
     include nebula::profile::apt
     include nebula::profile::authorized_keys
-    include nebula::profile::duo
     include nebula::profile::exim4
-    include nebula::profile::grub
     include nebula::profile::ntp
     include nebula::profile::tiger
-    include nebula::profile::users
     include nebula::profile::vim
+    include nebula::profile::aws::filesystem
     class { 'nebula::profile::networking':
-      bridge => $bridge_network,
-      keytab => true
+      bridge => false,
+      keytab => false
     }
   }
 
--- manifests/role/hathitrust.pp
+++ manifests/role/hathitrust.pp
@@ -7,22 +7,25 @@
 # @example
 #   include nebula::role::hathitrust
 class nebula::role::hathitrust {
+
   include nebula::profile::base
-  include nebula::profile::afs
-  include nebula::profile::authorized_keys
-  include nebula::profile::apt
-  include nebula::profile::duo
-  include nebula::profile::exim4
-  include nebula::profile::grub
-  include nebula::profile::ntp
-  include nebula::profile::tiger
-  include nebula::profile::users
-  include nebula::profile::vim
+  if $facts['os']['release']['major'] == '9' {
+    include nebula::profile::afs
+    include nebula::profile::apt
+    include nebula::profile::authorized_keys
+    include nebula::profile::duo
+    include nebula::profile::exim4
+    include nebula::profile::grub
+    include nebula::profile::ntp
+    include nebula::profile::tiger
+    include nebula::profile::users
+    include nebula::profile::vim
+    class { 'nebula::profile::networking':
+      bridge => false,
+      keytab => true
+    }
+  }
+
   include nebula::profile::dns::smartconnect
   include nebula::profile::elastic::metricbeat
-
-  class { 'nebula::profile::networking':
-    bridge => false,
-    keytab => true
-  }
 }
