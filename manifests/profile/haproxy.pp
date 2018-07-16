29fdd2742463114538291f4a465f623f6ff6ebeb
Aaron Elkiss <aelkiss@umich.edu>
2018-07-16 15:42:29 -0400

    pass through haproxy service parameters
    changes floating ips hash to 'services' hash that can include the
    floating IP parameter as well as throttling parameters

--- manifests/profile/haproxy.pp
+++ manifests/profile/haproxy.pp
@@ -7,7 +7,7 @@
 # @example
 #   include nebula::profile::haproxy
 class nebula::profile::haproxy(
-  Hash $floating_ips,
+  Hash $services,
   Hash $monitoring_user,
   String $cert_source = '',
 ) {
@@ -40,9 +40,9 @@ class nebula::profile::haproxy(
 
   $balanced_frontends.each |$service, $node_names| {
     nebula::haproxy_service { $service :
-      floating_ip => $floating_ips[$service],
       cert_source => $cert_source,
-      node_names  => $node_names
+      node_names  => $node_names,
+      *           => $services[$service]
     }
   }
 
