9e83e7d0e76e5bf2180d23ac579c2553b48994c5
Bryan Hockey <bhock@umich.edu>
2018-07-18 16:32:55 -0500

    Add and use minimum role

--- manifests/role/aws.pp
+++ manifests/role/aws.pp
@@ -8,17 +8,13 @@
 #   include nebula::role::aws
 class nebula::role::aws {
 
-  include nebula::profile::base
+  include nebula::role::minimum
   include nebula::profile::aws::filesystem
 
   if $facts['os']['release']['major'] == '9' {
-    include nebula::profile::apt
-    include nebula::profile::authorized_keys
     include nebula::profile::exim4
     include nebula::profile::ntp
     include nebula::profile::tiger
-    include nebula::profile::vim
-    include nebula::profile::aws::filesystem
     class { 'nebula::profile::networking':
       bridge => false,
       keytab => false
--- manifests/role/umich.pp
+++ manifests/role/umich.pp
@@ -10,18 +10,16 @@ class nebula::role::umich (
   $bridge_network = false,
 ) {
 
-  include nebula::profile::base
+  include nebula::role::minimum
+
   if $facts['os']['release']['major'] == '9' {
     include nebula::profile::afs
-    include nebula::profile::apt
-    include nebula::profile::authorized_keys
     include nebula::profile::duo
     include nebula::profile::exim4
     include nebula::profile::grub
     include nebula::profile::ntp
     include nebula::profile::tiger
     include nebula::profile::users
-    include nebula::profile::vim
     class { 'nebula::profile::networking':
       bridge => $bridge_network,
       keytab => true
