535cef6e9842a85effb25ec1ab714c81e5853a1e
Aaron Elkiss <aelkiss@umich.edu>
2018-07-18 11:04:52 -0400

    combine haproxy & keepalive profile

--- manifests/profile/haproxy.pp
+++ manifests/profile/haproxy.pp
@@ -9,9 +9,11 @@
 class nebula::profile::haproxy(
   Hash $services,
   Hash $monitoring_user,
+  Boolean $master = false,
   String $cert_source = '',
 ) {
   require nebula::profile::haproxy::prereqs
+  require nebula::profile::base::sysctl
 
   $balanced_frontends = balanced_frontends()
 
@@ -50,6 +52,36 @@ class nebula::profile::haproxy(
     gid  => 'haproxy',
     home => $monitoring_user['home'],
     key  => $monitoring_user['key']
+  }
+
+  package { 'keepalived': }
+  package { 'ipset': }
+
+  service { 'keepalived':
+    ensure     => 'running',
+    enable     => true,
+    hasrestart => true,
+    require    => Package['keepalived'],
+  }
+
+  $nodes_for_class = nodes_for_class($title)
+  $nodes_for_datacenter = nodes_for_datacenter($::datacenter)
+  $email = lookup('nebula::root_email')
+
+  file { '/etc/keepalived/keepalived.conf':
+    ensure  => 'present',
+    require => Package['keepalived'],
+    notify  => Service['keepalived'],
+    mode    => '0644',
+    content => template('nebula/profile/haproxy/keepalived/keepalived.conf.erb'),
+  }
+
+  file { '/etc/sysctl.d/keepalived.conf':
+    ensure  => 'present',
+    require => Package['keepalived'],
+    notify  => [Service['keepalived'], Service['procps']],
+    mode    => '0644',
+    content => template('nebula/profile/haproxy/keepalived/sysctl.conf.erb'),
   }
 
 }
