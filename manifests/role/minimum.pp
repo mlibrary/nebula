aaaa9a6d79faf68a95c5aacef6a4f7e2a5af4b3c
Matthew Alexander LaChance <mattlach@umich.edu>
2018-10-23 10:37:52 -0400

    AEIM-1074 - Ensure that state.yaml never gets bloated

--- manifests/role/minimum.pp
+++ manifests/role/minimum.pp
@@ -8,10 +8,11 @@
 #   include nebula::role::minimum
 class nebula::role::minimum {
   include nebula::profile::base
+  include nebula::profile::work_around_puppet_bugs
+
   if $facts['os']['release']['major'] == '9' {
     include nebula::profile::apt
     include nebula::profile::authorized_keys
     include nebula::profile::vim
   }
-
 }
