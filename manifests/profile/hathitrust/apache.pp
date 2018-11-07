b1bbe0e1cceb45cee8ee20f633e1f7cb13918c7b
Aaron Elkiss <aelkiss@umich.edu>
2018-11-07 16:20:50 -0500

    WIP: Translate  more of the apache config
    Most of what's remaining is deprecated vhosts, individual IPs that
    should be coming from hiera, or unclear how to achieve without using
    custom snippets. The next step is just giving it a try on a host.

--- manifests/profile/hathitrust/apache.pp
+++ manifests/profile/hathitrust/apache.pp
@@ -12,21 +12,17 @@ class nebula::profile::hathitrust::apache (
   String $gwt_code = ''
 ) {
 
-  # fcgi?
-
   class { 'apache':
     default_vhost          => false,
     default_ssl_vhost      => false,
-    # changed from default 300
+    # changed from default 300 (copypasta)
     timeout                => 900,
-    # changed from default 15 (puppet), 5 (debian)
+    # changed from default 15 (puppet), 5 (debian) (copypasta)
     keepalive_timeout      => 2,
     log_formats            => {
       combined => '%a %l %u %t "%r" %>s %b "%{Referer}i" "%{User-Agent}i" %v "%{X-HathiTrust-InCopyright}o"'
     },
-    docroot                => '/htapps/babel',
-    # configured below by explicitly declaring params for apache::mod::prefork
-    # class
+    # configured below by explicitly declaring params for apache::mod::prefork class
     mpm_module             => false,
     serveradmin            => 'lit-ae-systems@umich.edu',
     servername             => 'babel.hathitrust.org',
@@ -35,79 +31,17 @@ class nebula::profile::hathitrust::apache (
     # default is 'false'
     root_directory_secured => true,
     scriptalias            => undef,
-
-    # TODO virtual host defaults.. where to put these?
-    #    setenv            => ['SDRROOT /htapps/babel',
-    #                               'SDRDATAROOT /sdr1',
-    #                               # TODO use a better address
-    #                               'ASSERTION_EMAIL dlxs-system@umich.edu'],
-    #    setenvif          => ['Remote_Addr "::1" loopback',
-    #    'Remote_Addr "127.0.0.1" loopback'],
-    #    aliases           => { aliasmatch => '^/favicon.ico$',
-    #    path              => '/htapps/babel/common/web/favicon.ico' }
-    #    directoryindex    =>  "index.html"
+    docroot                => false,
   }
 
   class { 'apache::mod::prefork':
-    # copypasta from debian 8 config
+    # changed from default; copypasta from debian 8 config
     startservers           => 10,
     minspareservers        => 10,
     maxspareservers        => 15,
     maxrequestworkers      => 256,
     maxconnectionsperchild => 0
   }
-
-  # TODO
-
-  # ADD
-  # <Files ~ "~$">
-  #   Require all denied
-  # </Files>
-
-
-  # VERIFY (vhost-defaults.conf)
-  # <Directory />
-  #    AllowOverride None
-  #    Order deny,allow
-  #    Deny from all
-  # </Directory>
-  #
-  #
-  # ADD (vhost-defaults)
-  # <Directory "/htapps/babel">
-  #     Options None
-  #     AllowOverride None
-  #     Order deny,allow
-  #     Deny from all
-  # </Directory>
-  #
-  # ADD (vhost-defaults)
-  # # Grant access to necessary directories under the document root:
-  # # /htapps/babel/*/cgi
-  # # /htapps/babel/*/web
-  # # /htapps/babel/cache
-  # #
-  # # 2010-10-01 skorner
-  # <DirectoryMatch "^(/htapps/babel/(([^/]+)/(web|cgi)|widgets/([^/]+)/web|cache|mdp-web)/|/tmp/fastcgi/)(.*)">
-  #   Order allow,deny
-  #   Allow from all
-  #   Deny from env=badrobot
-  #   Deny from env=loadbalancer
-  # </DirectoryMatch>
-  #
-  # ADD (vhost-defaults)
-  # # Enable cgi execution under /htapps/babel/*/cgi.
-  #
-  # 2010-10-01 skorner
-  # <DirectoryMatch "^/htapps/babel/([^/]+)/cgi">
-  #      AllowOverride None
-  #      Options +ExecCGI
-  #      SetHandler cgi-script
-  # </DirectoryMatch>
-  #
-  # ADD (vhost-defaults)
-  # CustomLog "${APACHE_LOG_DIR}/access.log" combined env=!loopback
-
 
   # VERIFY no settings for <Directory /usr/share>
   # VERIFY no settings for <Directory /var/www>>
@@ -122,7 +56,6 @@ class nebula::profile::hathitrust::apache (
   # authz_host
   # autoindex TODO VERIFY DEFAULT CONFIG
   # cgi
-  # cosign TODO package as a deb
   # dir WITH CONFIG
   # env
   # expires
@@ -156,7 +89,6 @@ class nebula::profile::hathitrust::apache (
   #  # 2011-12-07 csnavely, skorner
   #  <Files *.sso>
   #    SetHandler shib-handler
-  #    CosignProtected Off
   #    Order allow,deny
   #    Allow from all
   #  </Files>
@@ -184,49 +116,146 @@ class nebula::profile::hathitrust::apache (
     requires => [] # TODO lookup from hiera
   }
 
-  apache::vhost { 'babel.hathitrust.org non-ssl':
-    servername      => 'babel.hathitrust.org',
+  apache::vhost { 'default non-ssl':
+    # TODO merge with top-level defaults
+    # ServerName localhost
+    # # Allow access only from localhost and haproxy, and send a redirect for / so monitoring doesn't get a 403.
+    # #
+    # # 2012-02-02 aelkiss
+    # <Directory "/">
+    # AllowOverride none
+    # Order deny,allow
+    # Deny from all
+    # Allow from 127.0.0.1
+    # # Allow from haproxy
+    # </Directory>
+    # # Additionally, allow access from grog & the squishees so they can check the index release & cache warming flags
+    # #
+    # # 2013-09-30 skorner
+    # <DirectoryMatch "^(/htapps/babel/(([^/]+)/(web|cgi)|widgets/([^/]+)/web|cache|mdp-web)/|/tmp/fastcgi/)(.*)">
+    # AllowOverride none
+    # Order deny,allow
+    # Deny from all
+    # Allow from 127.0.0.1
+    # # Allow from haproxy
+    # # Allow from grog
+    # # Allow from squishees
+    # </DirectoryMatch>
+    # RewriteEngine On
+    # RewriteRule ^(/$|/index.html$) https://babel.hathitrust.org/cgi/mb    [redirect=permanent,last]
+
+    servername         => 'localhost',
+
+    directoryindex     => 'index.html',
+    directories        => [
+      {
+        provider => 'filesmatch',
+        location =>  '~$',
+        require  => 'all denied'
+      },
+      {
+        provider       => 'directory',
+        location       => '/',
+        allow_override => ['None'],
+        require        =>  'all denied'
+      }
+    ],
+    manage_docroot     => false,
+    docroot            => '/htapps/babel',
+
+    setenvif           => [
+      'Remote_Addr "::1" loopback',
+      'Remote_Addr "127.0.0.1" loopback'
+    ],
+    access_log_file    => "\${APACHE_LOG_DIR}/access.log",
+    access_log_format  => 'combined',
+    access_log_env_var => 'env=!loopback',
+  }
+
+  ['babel', 'catalog', 'm', 'www'].each |String $vhost| {
+    apache::vhost { "${vhost}.hathitrust.org non-ssl":
+      servername        => $vhost,
+      docroot           => false,
+      port              => '80',
+      redirect_source   => '/',
+      redirect_status   => 'permanent',
+      redirect_dest     => "https://${vhost}.hathitrust.org",
+      access_log_file   => "\${APACHE_LOG_DIR}/${vhost}/access.log",
+      access_log_format => 'combined',
+      error_log_file    => "\${APACHE_LOG_DIR}/${vhost}/error.log"
+    }
+  }
+
+  apache::vhost { 'm.catalog.hathitrust.org redirection':
+    servername      => 'm.catalog.hathitrust.org',
+    docroot         => false,
     port            => '80',
-    docroot         => '/htapps/babel',
+    serveraliases   => ['m.catalog'],
+    redirect_source => '/',
     redirect_status => 'permanent',
-    redirect_dest   => 'https://babel.hathitrust.org',
+    redirect_dest   => 'https://m.hathitrust.org',
+  }
+
+  apache::vhost { 'hathitrust canonical name redirection':
+    servername      => 'hathitrust.org',
+    docroot         => false,
+    port            => '80',
+    serveraliases   => [
+      'www.hathitrust.com',
+      'hathitrust.com',
+      'www.hathitrust.info',
+      'hathitrust.info',
+      'www.hathitrust.net',
+      'hathitrust.net'
+    ],
+    redirect_source => '/',
+    redirect_status => 'permanent',
+    redirect_dest   => 'https://www.hathitrust.org',
   }
 
   apache::vhost { 'babel.hathitrust.org ssl':
-    servername    => 'babel.hathitrust.org',
-    serveraliases => [ 'ictc.babel.hathitrust.org',
-    'macc.babel.hathitrust.org',
-    'crms-training.babel.hathitrust.org' ],
-    port          => '443',
-    docroot       => '/htapps/babel',
-    ssl           => true,
-    ssl_cert      => '/etc/ssl/certs/www.hathitrust.org.crt',
-    ssl_key       => '/etc/ssl/private/www.hathitrust.org.key',
+    servername     => 'babel.hathitrust.org',
+    serveraliases  => [ 'crms-training.babel.hathitrust.org' ],
+    port           => '443',
+    docroot        => '/htapps/babel',
+    ssl            => true,
+    ssl_cert       => '/etc/ssl/certs/www.hathitrust.org.crt',
+    ssl_key        => '/etc/ssl/private/www.hathitrust.org.key',
 
-    # FROM COMMON
+    # from babel-common
 
-    aliases       => [
+    aliases        => [
       {
         aliasmatch => '^/robots.txt$',
         path       => '/htapps/babel/common/web/robots.txt'
       },
       {
-        # Support Google Webmaster Tools by making its verification file available at (2015-05-07 rrotter per roger)
+        # Support Google Webmaster Tools by making its verification file
+        # available at (2015-05-07 rrotter per roger)
         aliasmatch => '^google$gwt_code.html$',
         path       => "/htapps/babel/common/web/google${gwt_code}.html"
+      },
+      {
+        aliasmatch => '^/favicon.ico$',
+        path       => '/htapps/babel/common/web/favicon.ico'
       }
     ],
 
-    #
-    # TODO
-    # <If "%{HTTP_HOST} == 'crms-training.babel.hathitrust.org'">
-    #    SetEnv CRMS_INSTANCE crms-training
-    # </If>
-    # <Else>
-    #    SetEnv CRMS_INSTANCE production
-    # </Else>
+    directoryindex =>  'index.html',
 
-    rewrites      => [
+    setenv         => [
+      'SDRROOT /htapps/babel',
+      'SDRDATAROOT /sdr1',
+      # TODO use a better address
+      'ASSERTION_EMAIL dlxs-system@umich.edu'
+    ],
+
+    setenvifnocase => [
+      'Host crms-training\.babel\.hathitrust\.org CRMS_INSTANCE=crms-training',
+      'Host babel\.hathitrust\.org CRMS_INSTANCE=production'
+    ],
+
+    rewrites       => [
       {
         # Map web content URLs to the web directories within each application repository,
         # if the file being requested exists.
@@ -264,6 +293,7 @@ class nebula::profile::hathitrust::apache (
         # Fold ptsearch into pageturner. Remove after 12/31/2011
         #
         # 2010-04-26 aelkiss per roger
+        # 2018-11 - checked with roger -- retain indefinitely
         rewrite_rule => ['  ^/(shcgi|cgi)/ptsearch      /$1/pt/search        [redirect,noescape,last]'],
       },
 
@@ -326,17 +356,208 @@ class nebula::profile::hathitrust::apache (
         rewrite_rule => ['  ^(/$|/index.html$)      https://$hostname/cgi/mb  [redirect=permanent,last]'],
       },
 
-      # TODO
-      #   <Location "/">
-      #     AuthType    shibboleth
-      #     ShibRequestSetting  requireSession 0
-      #     require    shibboleth
-      #   </Location>
+    ],
 
-      ],
-
-      # TODO
-      # other virtual host configs
+    directories    => [
+      {
+        provider => 'filesmatch',
+        location =>  '~$',
+        require  => 'all denied'
+      },
+      {
+        provider       => 'directory',
+        location       => '/htapps/babel',
+        allow_override => ['None'],
+        require        =>  'all denied'
+      },
+      {
+        provider              => 'location',
+        path                  => '/',
+        auth_type             => 'shibboleth',
+        require               => 'shibboleth',
+        shib_request_settings => { 'requireSession' => '0'}
+      },
+      {
+        # Grant access to necessary directories under the document root:
+        # /htapps/babel/*/cgi
+        # /htapps/babel/*/web
+        # /htapps/babel/cache
+        #
+        # 2010-10-01 skorner
+        provider => 'directorymatch',
+        location => '^(/htapps/babel/(([^/]+)/(web|cgi)|widgets/([^/]+)/web|cache|mdp-web)/|/tmp/fastcgi/)(.*)">',
+        require  => {
+          enforce  => 'all',
+          requires => [
+            'not env badrobot',
+            'not env loadbalancer'
+          ]
+        }
+      },
+      {
+        # Enable cgi execution under /htapps/babel/*/cgi.
+        #
+        # 2010-10-01 skorner
+        provider       => 'directorymatch',
+        location       => '^/htapps/babel/([^/]+)/cgi',
+        allow_override => 'None',
+        options        => '+ExecCGI',
+        sethandler     =>  'cgi-script'
+      }
+    ]
 
   }
+
+  # TODO:
+  # - default virtual host (for monitoring)
+  # - catalog virtual host - DONE
+  # - m.babel (deprecate?)
+  # - mdp.lib (deprecate?)
+  # - www
+  # - production sites (catalog aliases, m.hathitrust.org redirect) - DONE
+  apache::vhost { 'catalog.hathitrust.org ssl':
+    servername        => 'https://catalog.hathitrust.org',
+    serveraliases     => ['m.hathitrust.org'],
+    manage_docroot    => false,
+    docroot           => '/htapps/catalog/web',
+    error_log_file    => "\${APACHE_LOG_DIR}/catalog/error.log",
+    access_log_file   => '\%{APACHE_LOG_DIR}/catalog/access.log',
+    access_log_format => 'combined',
+    directoryindex    => 'index.html index.htm index.php index.phtml index.shtml',
+
+    directories       => [
+      {
+        provider => 'filesmatch',
+        location =>  '~$',
+        require  => 'all denied'
+      },
+      {
+        provider       => 'directory',
+        path           => '/htapps/catalog/web',
+        options        => ['FollowSymlinks'],
+        allow_override => ['all'],
+        require        => {
+          enforce  => 'all',
+          requires => [
+            'not env badrobot',
+            'not env loadbalancer'
+          ]
+        }
+      },
+      {
+        provider => 'directory',
+        path     =>  '/htapps/babel/common/web',
+        require  => {
+          enforce  => 'all',
+          requires => [
+            'not env badrobot',
+            'not env loadbalancer'
+          ]
+        }
+      },
+    ],
+
+    aliases           => [
+      {
+        aliasmatch => '^/favicon.ico$',
+        path       => '/htapps/babel/common/web/favicon.ico'
+      },
+      {
+        alias => '/common/',
+        path  => '/htapps/babel/common/web/'
+      }
+    ],
+
+    rewrites          => [
+      {
+
+        # redirect top-level page to www.hathitrust.org, but not for mobile or orphanworks host names
+        #
+        # 2010-11-12 csnavely per jjyork
+        #
+        # adapted to take effect for catalog.hathitrust.org only after consolidating m.hathitrust.org and
+        # orphanworks.hathtrust.org into this virtual host
+        #
+        # 2012-04-17 skorner per dueberb
+
+        rewrite_cond => '%{HTTP_HOST}    ^(catalog|test\.catalog)  [nocase]',
+        rewrite_rule => '^(/$|/index.html$)  https://www.hathitrust.org/  [redirect=permanent,last]'
+
+      }
+    ]
+  }
+
+  apache::vhost { 'www.hathitrust.org ssl':
+    servername        => 'https://www.hathitrust.org',
+    manage_docroot    => false,
+    docroot           => '/htapps/www',
+    error_log_file    => "\${APACHE_LOG_DIR}/www/error.log",
+    access_log_file   => "\${APACHE_LOG_DIR}/www/access.log",
+    access_log_format => 'combined',
+    setenv            => ['SDRROOT /htapps/www'],
+    directoryindex    => 'index.html index.htm index.php index.phtml index.shtml',
+
+    directories       => [
+      {
+        provider => 'filesmatch',
+        location =>  '~$',
+        require  => 'all denied'
+      },
+      {
+        provider       => 'directory',
+        path           => '/htapps/www',
+        options        => ['IncludesNoExec','Indexes','FollowSymLinks','MultiViews'],
+        allow_override => ['AuthConfig','FileInfo','Limit','Options'],
+        require        => {
+          enforce  => 'all',
+          requires => [
+            'not env badrobot',
+            'not env loadbalancer'
+          ]
+        }
+      },
+      {
+        provider => 'directory',
+        path     =>  '/htapps/babel/common/web',
+        require  => {
+          enforce  => 'all',
+          requires => [
+            'not env badrobot',
+            'not env loadbalancer'
+          ]
+        }
+      },
+    ],
+
+    aliases           => [
+      {
+        aliasmatch => '^/favicon.ico$',
+        path       => '/htapps/babel/common/web/favicon.ico'
+      },
+      {
+        alias => '/common/',
+        path  => '/htapps/babel/common/web/'
+      }
+    ],
+
+    rewrites          => [
+      {
+        # Serve static assets through apache
+        rewrite_cond => '/htapps/apps/usdocs_registry/public/$1 -f',
+        rewrite_rule =>  '^/usdocs_registry/(.*)$  /htapps/apps/usdocs_registry/public/$1 [L]',
+      }
+    ],
+
+    proxy_pass        => [
+      {
+        path   => '/usdocs_registry',
+        url    => 'http://apps-ht:30001/',
+        params => { 'retry' => '2' }
+      }
+    ],
+
+    headers           => 'Header set "Strict-Transport-Security" "max-age=31536000"',
+
+  }
+
 }
