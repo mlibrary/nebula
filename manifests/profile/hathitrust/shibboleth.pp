d0d4a5506061591817dbf3c5037856f09d818588
Aaron Elkiss <aelkiss@umich.edu>
2018-11-08 13:16:54 -0500

    add shibboleth configuration
    for now, just puts static config files in place
    still to do: set permissions so shibd can read SSL keys

--- manifests/profile/hathitrust/shibboleth.pp
+++ manifests/profile/hathitrust/shibboleth.pp
@@ -16,7 +16,35 @@ class nebula::profile::hathitrust::shibboleth () {
 # not in debian 9 - would need to install via https://downloads.mariadb.org/connector-odbc/
 #      'libmyodbc'
       'unixodbc',
-      'libapache2-mod-shib2'
+      'libapache2-mod-shib2',
+      'shibboleth-sp2-common',
+      'shibboleth-sp2-utils'
     ]:
   }
+
+  service { 'shibd':
+    ensure     => 'running',
+    enable     => true,
+    hasrestart => true,
+    require    => Package['shibboleth-sp2-utils'],
+  }
+
+  file { "/etc/shibboleth":
+    ensure  => 'directory',
+    mode    => '0775',
+    owner   => 'root',
+    group   => 'root',
+    recurse => true,
+    purge   => true,
+    links   => 'follow',
+    notify  => Service['shibd'],
+    source  => "puppet:///shibboleth"
+  }
+
+  file { "/etc/shibboleth/shibboleth2.xml":
+    mode  => '0440',
+    owner => '_shibd',
+    group => 'nogroup'
+  }
+
 }
