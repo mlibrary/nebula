faaddd1ecd54b3130f43c096160e62c18c78adf1
Noah Botimer <botimer@umich.edu>
2018-11-13 16:35:17 -0500

    AEIM-1430 - Install PEAR modules with puppet/php
    This attempts to install all of the PEAR modules through the
    'puppet/php' module, which appears to be the most mature and maintained
    on the forge. The usage does not install FPM, dev headers, or composer,
    but each is simple to add. I chose to remove the Debian packages for the
    select few PEAR modules and list them all as extensions on the PHP
    resource.

--- manifests/profile/hathitrust/php.pp
+++ manifests/profile/hathitrust/php.pp
@@ -18,27 +18,52 @@ class nebula::profile::hathitrust::php () {
       'php7.0-common',
       'php7.0-curl',
       'php7.0-gd',
-      'php-geoip',
+      'php-geoip', # PECL
       'php7.0-ldap',
       'php7.0-mysql',
       'php7.0-xsl',
-      'php-date',
-      'php-db',
-      'php-http-request',
-      'php-log',
-      'php-mail',
-      'php-mdb2',
-      'php-net-smtp',
-      'php-net-url2',
       'php-pear',
       'libapache2-mod-php7.0',
       'pear-channels'
     ]:
   }
 
-#      to install via pear
-#      'php-pager',
-#      'php-xml-parser',
-#      'php-xml-serializer',
+  class { '::php':
+    ensure => present,     # Don't touch stuff from above; should be equivalent
+    manage_repos => false, # Set true to add dotdeb repos
+    fpm => false,          # We only use mod_php at present
+    dev => false,          # Extensions are only added by PECL debs for now
+    composer => false,     # System-wide composer seems iffy unless using dotdeb
+    pear => true,          # We're using this for PEAR, so set to true
+    phpunit => true,       # Unsure whether this should be system or app-level
 
+    extensions => {
+      Archive_Tar:           { ensure => '1.4.3',   package_prefix => '', provider => 'pear' },
+      Console_Getopt:        { ensure => '1.4.1',   package_prefix => '', provider => 'pear' },
+      DB:                    { ensure => '1.7.14',  package_prefix => '', provider => 'pear' },
+      DB_DataObject:         { ensure => '1.11.2',  package_prefix => '', provider => 'pear' },
+      Date:                  { ensure => '1.4.7',   package_prefix => '', provider => 'pear' },
+      File_MARC:             { ensure => '1.1.1',   package_prefix => '', provider => 'pear' },
+      HTTP_Request:          { ensure => '1.4.4',   package_prefix => '', provider => 'pear' },
+      HTTP_Request2:         { ensure => '2.2.0',   package_prefix => '', provider => 'pear' },
+      HTTP_Session2:         { ensure => '0.7.3',   package_prefix => '', provider => 'pear' },
+      Log:                   { ensure => '1.12.8',  package_prefix => '', provider => 'pear' },
+      MDB2:                  { ensure => '2.5.0b5', package_prefix => '', provider => 'pear' },
+      MDB2_Driver_mysql:     { ensure => '1.4.1',   package_prefix => '', provider => 'pear' },
+      Mail:                  { ensure => '1.2.0',   package_prefix => '', provider => 'pear' },
+      Net_SMTP:              { ensure => '1.6.2',   package_prefix => '', provider => 'pear' },
+      Net_Socket:            { ensure => '1.0.14',  package_prefix => '', provider => 'pear' },
+      Net_URL:               { ensure => '1.0.15',  package_prefix => '', provider => 'pear' },
+      Net_URL2:              { ensure => '2.0.7',   package_prefix => '', provider => 'pear' },
+      PEAR:                  { ensure => '1.10.5',  package_prefix => '', provider => 'pear' },
+      Pager:                 { ensure => '2.4.8',   package_prefix => '', provider => 'pear' },
+      PhpDocumentor:         { ensure => '1.4.4',   package_prefix => '', provider => 'pear' },
+      Structures_DataGrid:   { ensure => '0.9.3',   package_prefix => '', provider => 'pear' },
+      Structures_Graph:      { ensure => '1.1.1',   package_prefix => '', provider => 'pear' },
+      Structures_LinkedList: { ensure => '0.2.2',   package_prefix => '', provider => 'pear' },
+      XML_Parser:            { ensure => '1.3.4',   package_prefix => '', provider => 'pear' },
+      XML_Serializer:        { ensure => '0.20.2',  package_prefix => '', provider => 'pear' },
+      XML_Util:              { ensure => '1.4.2',   package_prefix => '', provider => 'pear' },
+    },
+  }
 }
