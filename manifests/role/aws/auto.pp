da0cc98772f4bee1d051d0bc90b27e063d92e658
Ryan Rotter <rrotter@umich.edu>
2018-12-19 11:41:13 -0600

    Add aws::auto role to automatically select roles by EC2 tag

---
+++ manifests/role/aws/auto.pp
@@ -0,0 +1,17 @@
+# Copyright (c) 2019 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+
+# Include basic aws role and the role listed for the node in EC2 tag "role"
+#
+# @example
+#   include nebula::role::aws::auto
+class nebula::role::aws::auto {
+  include nebula::role::aws
+
+  if $::ec2_tag_role {
+    include Class[$::ec2_tag_role]
+  } else {
+    fail('no ec2 role tag defined')
+  }
+}
