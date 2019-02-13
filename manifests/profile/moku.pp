4cfde7e750ed32b84e84e66869b4815fd5426af4
Matthew Alexander LaChance <mattlach@umich.edu>
2019-02-13 13:56:05 -0500

    AEIM-1762 - Begin templating the init json

--- manifests/profile/moku.pp
+++ manifests/profile/moku.pp
@@ -7,4 +7,21 @@
 # @example
 #   include nebula::profile::moku
 class nebula::profile::moku {
+  lookup('nebula::named_instances').each |$key, $instance| {
+    # Collect exported `moku init` exec resources. These are exported by
+    # named_instance resources.
+    Exec <<| command == "moku init < '/tmp/.moku_init_${key}.json'" |>>
+
+    $path = $instance['path']
+    if has_key($instance, 'subservices') {
+      $subservices = $instance['subservices']
+    } else {
+      $subservices = []
+    }
+
+    file { "/tmp/.moku_init_${key}.json":
+      ensure  => 'present',
+      content => template('nebula/profile/moku/init.json.erb'),
+    }
+  }
 }
--- spec/classes/profile/moku_spec.rb
+++ spec/classes/profile/moku_spec.rb
@@ -8,7 +8,36 @@ require 'spec_helper'
 describe 'nebula::profile::moku' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
+      let(:hiera_config) { 'spec/fixtures/hiera/named_instances_config.yaml' }
+      let(:facts) { os_facts }
+
       it { is_expected.to compile }
+
+      describe '/tmp/.moku_init_first-instance.json' do
+        let(:file) { '/tmp/.moku_init_first-instance.json' }
+
+        it { is_expected.to contain_file(file).with_ensure('present') }
+
+        [
+          %r{^\s*"deploy_dir": "/www-invalid/first-instance/app",$},
+          %r{^\s*"systemd_services": \["one_subservice", "another_subservice"\],$},
+        ].each do |search_string|
+          it { is_expected.to contain_file(file).with_content(search_string) }
+        end
+      end
+
+      describe '/tmp/.moku_init_minimal-instance.json' do
+        let(:file) { '/tmp/.moku_init_minimal-instance.json' }
+
+        it { is_expected.to contain_file(file).with_ensure('present') }
+
+        [
+          %r{^\s*"deploy_dir": "/www-invalid/minimal/app",$},
+          %r{^\s*"systemd_services": \[\],$},
+        ].each do |search_string|
+          it { is_expected.to contain_file(file).with_content(search_string) }
+        end
+      end
     end
   end
 end
