4b0c0a6d076fbeb222e4b8124df4709f7d8e5f56
Matthew Alexander LaChance <mattlach@umich.edu>
2019-02-18 10:30:14 -0500

    AEIM-1762 - Add all the json except the infrastructure

--- manifests/profile/moku.pp
+++ manifests/profile/moku.pp
@@ -6,22 +6,92 @@
 #
 # @example
 #   include nebula::profile::moku
-class nebula::profile::moku {
-  lookup('nebula::named_instances').each |$key, $instance| {
-    # Collect exported `moku init` exec resources. These are exported by
-    # named_instance resources.
-    Exec <<| command == "moku init < '/tmp/.moku_init_${key}.json'" |>>
-
+class nebula::profile::moku (
+  String $init_directory = '/etc/moku/init',
+) {
+  lookup('nebula::named_instances').each |$name, $instance| {
+    $url = $instance['url']
     $path = $instance['path']
-    if has_key($instance, 'subservices') {
-      $subservices = $instance['subservices']
+
+    if has_key($instance, 'users') {
+      $users = to_json($instance['users'])
     } else {
-      $subservices = []
+      $users = '[]'
     }
 
-    file { "/tmp/.moku_init_${key}.json":
-      ensure  => 'present',
-      content => template('nebula/profile/moku/init.json.erb'),
+    if has_key($instance, 'subservices') {
+      $subservices = to_json($instance['subservices'])
+    } else {
+      $subservices = '[]'
+    }
+
+    concat_file { "${name} deploy init":
+      path   => "${init_directory}/${name}.json",
+      format => 'json-pretty',
+    }
+
+    Concat_fragment <<| target == "${name} deploy init" |>>
+
+    concat_fragment {
+      default:
+        target  => "${name} deploy init",
+        ;
+
+      "${name} deploy init instance.source.url":
+        content => "{\"instance\": {\"source\": {\"url\": \"${url}\"}}}",
+        ;
+
+      "${name} deploy init instance.source.commitish":
+        content => '{"instance": {"source": {"commitish": "master"}}}',
+        ;
+
+      "${name} deploy init instance.deploy.url":
+        content => '{"instance": {"deploy": {"url": "git@github.com:mlibrary/moku-deploy"}}}',
+        ;
+
+      "${name} deploy init instance.deploy.commitish":
+        content => "{\"instance\": {\"deploy\": {\"commitish\": \"${name}\"}}}",
+        ;
+
+      "${name} deploy init instance.infrastructure.url":
+        content => '{"instance": {"infrastructure": {"url": "git@github.com:mlibrary/moku-infrastructure"}}}',
+        ;
+
+      "${name} deploy init instance.infrastructure.commitish":
+        content => "{\"instance\": {\"infrastructure\": {\"commitish\": \"${name}\"}}}",
+        ;
+
+      "${name} deploy init instance.dev.url":
+        content => '{"instance": {"dev": {"url": "git@github.com:mlibrary/moku-dev"}}}',
+        ;
+
+      "${name} deploy init instance.dev.commitish":
+        content => "{\"instance\": {\"dev\": {\"commitish\": \"${name}\"}}}",
+        ;
+
+      "${name} deploy init permissions.deploy":
+        content => "{\"permissions\": {\"deploy\": ${users}}}",
+        ;
+
+      "${name} deploy init permissions.edit":
+        content => "{\"permissions\": {\"edit\": ${users}}}",
+        ;
+
+      "${name} deploy init deploy.deploy_dir":
+        content => "{\"deploy\": {\"deploy_dir\": \"${path}\"}}",
+        ;
+
+      "${name} deploy init deploy.env.rack_env":
+        content => '{"deploy": {"env": {"rack_env": "production"}}}',
+        ;
+
+      "${name} deploy init deploy.systemd_services":
+        content => "{\"deploy\": {\"systemd_services\": ${subservices}}}",
+        ;
+
+      "${name} deploy init deploy.sites.user":
+        content => "{\"deploy\": {\"sites\": {\"user\": \"${name}\"}}}",
+        ;
     }
   }
 }
