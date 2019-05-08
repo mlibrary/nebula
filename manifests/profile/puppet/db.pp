329be677b99d0711858e5221c099f11e2abdec97
Matthew Alexander LaChance <mattlach@umich.edu>
2019-05-08 09:56:55 -0400

    Allow hiera to control puppetdb threads

--- manifests/profile/puppet/db.pp
+++ manifests/profile/puppet/db.pp
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 The Regents of the University of Michigan.
+# Copyright (c) 2018-2019 The Regents of the University of Michigan.
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
 
@@ -6,9 +6,14 @@
 #
 # @example
 #   include nebula::profile::puppet::db
-class nebula::profile::puppet::db {
+class nebula::profile::puppet::db (
+  $command_threads = undef,
+  $concurrent_writes = undef,
+) {
   class { 'puppetdb':
     disable_cleartext => true,
     manage_firewall   => false,
+    command_threads   => $command_threads,
+    concurrent_writes => $concurrent_writes,
   }
 }
--- spec/classes/profile/puppet/db_spec.rb
+++ spec/classes/profile/puppet/db_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Copyright (c) 2018 The Regents of the University of Michigan.
+# Copyright (c) 2018-2019 The Regents of the University of Michigan.
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
 require 'spec_helper'
@@ -14,7 +14,21 @@ describe 'nebula::profile::puppet::db' do
         is_expected.to contain_class('puppetdb').with(
           disable_cleartext: true,
           manage_firewall: false,
+          command_threads: nil,
+          concurrent_writes: nil,
         )
+      end
+
+      context 'with command_threads set to 8' do
+        let(:params) { { command_threads: 8 } }
+
+        it { is_expected.to contain_class('puppetdb').with_command_threads(8) }
+      end
+
+      context 'with concurrent_writes set to 8' do
+        let(:params) { { concurrent_writes: 8 } }
+
+        it { is_expected.to contain_class('puppetdb').with_concurrent_writes(8) }
       end
     end
   end
