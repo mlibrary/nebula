17dde01b61cd7b1e918f37791867d91469a5ca54
Matthew Alexander LaChance <mattlach@umich.edu>
2019-05-08 10:40:36 -0400

    Enable reporting to puppetdb

--- manifests/profile/puppet/master_with_db.pp
+++ manifests/profile/puppet/master_with_db.pp
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 The Regents of the University of Michigan.
+# Copyright (c) 2018-2019 The Regents of the University of Michigan.
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
 
@@ -10,6 +10,8 @@ class nebula::profile::puppet::master_with_db (
   String $puppetdb_server = lookup('nebula::puppetdb'),
 ) {
   class { 'puppetdb::master::config':
-    puppetdb_server => $puppetdb_server,
+    puppetdb_server         => $puppetdb_server,
+    manage_report_processor => true,
+    enable_reports          => true,
   }
 }
