844d9c39d7dda168e6c786895b071e7fde08a169
Bryan Hockey <bhock@umich.edu>
2019-07-01 18:30:06 -0500

    PFDR-178: Add chipmunk role

---
+++ manifests/role/chipmunk.pp
@@ -0,0 +1,8 @@
+class nebula::role::chipmunk {
+  include nebula::role::umdl
+  include nebula::profile::users
+  include nebula::profile::hathitrust::dependencies
+  include nebula::profile::hathitrust::perl
+  include nebula::profile::nodejs
+  include nebula::profile::named_instances
+}
