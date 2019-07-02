f8dcf260db73b500b916171929aa962c75cb5b26
Aaron Elkiss <aelkiss@umich.edu>
2019-07-02 15:47:29 -0400

    PFDR-222: inherit from nebula::role::app_host::prod

--- manifests/role/chipmunk.pp
+++ manifests/role/chipmunk.pp
@@ -1,8 +1,5 @@
 class nebula::role::chipmunk {
-  include nebula::role::umich
-  include nebula::profile::users
+  include nebula::role::app_host::prod
   include nebula::profile::hathitrust::dependencies
   include nebula::profile::hathitrust::perl
-  include nebula::profile::nodejs
-  include nebula::profile::named_instances
 }
