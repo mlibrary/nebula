f2cbe7eaa3de61917374d47576283bd70efbe40d
Matthew Alexander LaChance <mattlach@umich.edu>
2019-07-06 16:07:23 -0400

    Rewrite ssh firewall to use exposed_port resource

--- manifests/profile/networking/firewall/ssh.pp
+++ manifests/profile/networking/firewall/ssh.pp
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 The Regents of the University of Michigan.
+# Copyright (c) 2018-2019 The Regents of the University of Michigan.
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
 
@@ -8,17 +8,9 @@
 #
 # @example
 #   include nebula::profile::networking::firewall::ssh
-class nebula::profile::networking::firewall::ssh (
-  Array $networks = [],
-) {
-
-  $networks.flatten.each |$network| {
-    firewall { "100 SSH: ${network['name']}":
-      proto  => 'tcp',
-      dport  => 22,
-      source => $network['block'],
-      state  => 'NEW',
-      action => 'accept',
-    }
+class nebula::profile::networking::firewall::ssh {
+  nebula::exposed_port { '100 SSH':
+    port  => 22,
+    block => 'umich::networks::all_trusted_machines',
   }
 }
