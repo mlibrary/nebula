6c6320681130dd9fabd9aa166671b5274a0682f8
Bryan Hockey <bhock@umich.edu>
2019-07-10 11:41:40 -0500

    Add redis, mysql

---
+++ spec/classes/profile/mysql_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2019 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+require 'spec_helper'
+
+describe 'nebula::profile::mysql' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+      let(:params) { { password: 'some_password' } }
+
+      it { is_expected.to compile }
+    end
+  end
+end
