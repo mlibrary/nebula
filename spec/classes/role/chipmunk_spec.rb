d4702cc20b4fb50d692c708efc779563f438cb52
Aaron Elkiss <aelkiss@umich.edu>
2019-07-11 16:37:26 -0400

    add default mysql password to hiera fixture

--- spec/classes/role/chipmunk_spec.rb
+++ spec/classes/role/chipmunk_spec.rb
@@ -9,6 +9,7 @@ describe 'nebula::role::chipmunk' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
+      let(:hiera_config) { 'spec/fixtures/hiera/chipmunk_config.yaml' }
 
       it { is_expected.to compile }
     end
