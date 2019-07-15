e57069a896ecd526980d131ee4a62f7e833136de
Aaron Elkiss <aelkiss@umich.edu>
2019-07-15 07:35:32 -0400

    AEIM-1705: php7.3 for www.lib

--- manifests/role/mgetit_log.pp
+++ manifests/role/mgetit_log.pp
@@ -11,4 +11,5 @@ class nebula::role::mgetit_log {
   include nebula::profile::elastic::filebeat::prospectors::mgetit
   include nebula::profile::named_instances
   include nebula::profile::nodejs
+  include nebula::profile::php73
 }
