ba1773fc4ad034e40ba0a7695a487cc36568eec2
Aaron Elkiss <aelkiss@umich.edu>
2019-08-19 09:26:29 -0400

    role for ht backup host

---
+++ manifests/role/hathitrust/backup.pp
@@ -0,0 +1,21 @@
+# Copyright (c) 2018 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+
+# backup servers for hathitrust.org
+#
+# @example
+#   include nebula::role::hathitrust::backup
+class nebula::role::hathitrust::backup (String $private_address_template = '192.168.0.%s') {
+  include nebula::role::hathitrust
+
+  class { 'nebula::profile::networking::private':
+    address_template => $private_address_template
+  }
+
+  class { 'nebula::profile::hathitrust::mounts':
+    smartconnect_mounts => ['/htapps','/htprep'],
+    readonly            => true
+  }
+
+}
