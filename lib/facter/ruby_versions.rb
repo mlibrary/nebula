fe7ff9113773a94b8a836aa58c9e279011aba6f0
Ryan Rotter <rrotter@umich.edu>
2019-09-19 15:09:05 -0500

    Make _working_ version of ruby_versions fact

--- lib/facter/ruby_versions.rb
+++ lib/facter/ruby_versions.rb
@@ -4,7 +4,7 @@ Facter.add(:ruby_versions) do
   setcode do
     if File.exist?('/etc/profile.d/rbenv.sh')
       Facter::Core::Execution.execute(
-        "source /etc/profile.d/rbenv.sh; rbenv versions --bare --skip-aliases",
+        '/bin/bash -c "source /etc/profile.d/rbenv.sh; rbenv versions --bare --skip-aliases"',
       ).strip.split("\n")
     else
       []
