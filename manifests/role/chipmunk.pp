c4e03783effdbc5a07f8c3725f230873aa5ad708
Noah Botimer <botimer@umich.edu>
2019-10-10 17:38:07 -0400

    Condition detailed chipmunk/perl deps on OS release

--- manifests/role/chipmunk.pp
+++ manifests/role/chipmunk.pp
@@ -1,8 +1,11 @@
 class nebula::role::chipmunk {
   include nebula::role::app_host::standalone
 
-  include nebula::profile::hathitrust::dependencies
-  include nebula::profile::hathitrust::perl
+  # Make it possible for the catalog to apply on jessie while we are upgrading.
+  if $facts['os']['family'] == 'Debian' and $::lsbdistcodename != 'jessie' {
+    include nebula::profile::hathitrust::dependencies
+    include nebula::profile::hathitrust::perl
+  }
 
   # should be conditionally included from named_instances::apache when the
   # vhost config is deployed on the web node
