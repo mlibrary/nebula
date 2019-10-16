008b2a4bb825050d9cc45cbb6deaa026dff54955
Matthew Alexander LaChance <mattlach@umich.edu>
2019-10-16 13:23:31 -0400

    Add nebula::role::archivematica

---
+++ manifests/role/archivematica.pp
@@ -0,0 +1,27 @@
+# Copyright (c) 2019 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+
+# Archivematica
+#
+# This is an ubuntu server and is almost entirely managed through
+# ansible instead of puppet. All puppet does is manage authorized keys
+# and the firewall.
+class nebula::role::archivematica {
+  include nebula::role::minimum
+
+  nebula::exposed_port { '200 HTTP Dashboard':
+    port  => 80,
+    block => 'umich::networks::campus_wired_and_wireless',
+  }
+
+  nebula::exposed_port { '200 HTTP Storage LIT':
+    port  => 8000,
+    block => 'umich::networks::staff',
+  }
+
+  nebula::exposed_port { '200 HTTP Storage Bentley':
+    port  => 8000,
+    block => 'umich::networks::bentley',
+  }
+}
