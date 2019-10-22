968a6553a52d50396c96a3c2eb66684d9da5aed4
Suleman Diwan <suledwan@umich.edu>
2019-10-22 16:19:53 -0400

    Removed purge from the ipv6 chain stanza

--- manifests/profile/networking/firewall.pp
+++ manifests/profile/networking/firewall.pp
@@ -97,7 +97,6 @@ class nebula::profile::networking::firewall (
 
   # Disable IPv6  
   firewallchain { 'INPUT:filter:IPv6':
-    purge => true,
     ensure => present,
     policy => drop,
     before => undef,
