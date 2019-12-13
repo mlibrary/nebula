e2f186ffcd65884472fc4d30d06147ea2785d2c9
Matthew Alexander LaChance <mattlach@umich.edu>
2019-12-13 15:49:31 -0500

    Add kubectl to deploy hosts

--- manifests/profile/moku.pp
+++ manifests/profile/moku.pp
@@ -18,4 +18,18 @@ class nebula::profile::moku (
     lookup('nebula::named_instances'),
     { init_directory =>  $init_directory }
   )
+
+  package { 'kubectl':
+    require => Apt::Source['kubernetes'],
+  }
+
+  apt::source { 'kubernetes':
+    location => 'https://apt.kubernetes.io/',
+    release  => "kubernetes-${::lsbdistcodename}",
+    repos    => 'main',
+    key      => {
+      'id'     => '54A647F9048D5688D7DA2ABE6A030B21BA07F4FB',
+      'source' => 'https://packages.cloud.google.com/apt/doc/apt-key.gpg',
+    },
+  }
 }
