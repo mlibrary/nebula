45485601b177c19d163460a4b4c267619fd767f0
Suleman Diwan <suledwan@umich.edu>
2019-12-20 12:17:46 -0500

    Travis fixes

--- spec/classes/role/archivematica_spec.rb
+++ spec/classes/role/archivematica_spec.rb
@@ -8,12 +8,10 @@ require 'spec_helper'
 describe 'nebula::role::archivematica' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
-
       let(:facts) { os_facts }
       let(:hiera_config) { 'spec/fixtures/hiera/archivematica_config.yaml' }
 
       it { is_expected.to compile }
-
     end
   end
 end
