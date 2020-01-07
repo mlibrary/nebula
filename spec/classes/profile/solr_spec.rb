80c957c18f3f317643bc30ba7159128ace0684d9
Suleman Diwan <suledwan@umich.edu>
2020-01-07 18:04:01 -0500

    Fix it-do issues

--- spec/classes/profile/solr_spec.rb
+++ spec/classes/profile/solr_spec.rb
@@ -13,60 +13,52 @@ describe 'nebula::profile::solr' do
       it { is_expected.to compile }
 
       # Packages
-      it 'install packages' do
-        [
-          'openjdk-8-jre-headless',
-          'solr',
-          'lsof',
-        ].each do |package|
-          it { is_expected.to contain_package(package) }
-        end
+      [
+        'openjdk-8-jre-headless',
+        'solr',
+        'lsof',
+      ].each do |package|
+        it { is_expected.to contain_package(package) }
       end
 
       # Directories
-      it 'include directories with proper permissions' do
-        [
-          '/var/lib/solr',
-          '/var/lib/solr/home',
-          '/var/lib/solr/logs',
-        ].each do |path|
-          it do
-            is_expected.to contain_file(path).with(
-              owner: 'solr',
-              group: 'solr',
-              ensure: 'directory',
-              mode: '0750',
-            )
-          end
+      [
+        '/var/lib/solr',
+        '/var/lib/solr/home',
+        '/var/lib/solr/logs',
+      ].each do |path|
+        it do
+          is_expected.to contain_file(path).with(
+            owner: 'solr',
+            group: 'solr',
+            ensure: 'directory',
+            mode: '0750',
+          )
         end
       end
 
       # Files
-      it 'include files with proper permissions' do
-        [
-          '/var/lib/solr/log4j.properties',
-          '/var/lib/solr/solr.in.sh',
-          '/var/lib/solr/home/solr.xml',
-        ].each do |path|
-          it do
-            is_expected.to contain_file(path).with(
-              owner: 'solr',
-              group: 'solr',
-              ensure: 'file',
-              mode: '0644',
-            )
-          end
+      [
+        '/var/lib/solr/log4j.properties',
+        '/var/lib/solr/solr.in.sh',
+        '/var/lib/solr/home/solr.xml',
+      ].each do |path|
+        it do
+          is_expected.to contain_file(path).with(
+            owner: 'solr',
+            group: 'solr',
+            ensure: 'file',
+            mode: '0644',
+          )
         end
       end
 
       # Service
-      it 'start the solr service' do
-        it do
-          is_expected.to contain_service('solr').with(
-            enable: true,
-            ensure: 'running',
-          )
-        end
+      it do
+        is_expected.to contain_service('solr').with(
+          enable: true,
+          ensure: 'running',
+        )
       end
     end
   end
