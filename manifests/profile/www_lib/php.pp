a50f950de43ede252828d538e1c7d4397e5ef078
Suleman Diwan <suledwan@umich.edu>
2020-05-13 17:43:55 -0400

    AEIM-2778 - Add php configuration to www-lib profile
    Add php and php-fpm config to the www-lib profile that is independent of
    other php configs.
    
      - Add php repo and package installs.
      - Configure PHP, FPM, Apache PHP Module.
      - Disable default 'www' fpm pool creation; instead manually create
        it using php::fpm::pools.
      - Update role for www-lib to use new setup.
      - Add global php version default.
      - parameterize php version (default is 7.3).
    
    Mac specific php puppet environment issues:
    
      - Add ip6tables_version to default_facts to circumvent error on Mac
        due to nil reference in some versions of php puppet module.
      - Add Mac platform to Gemfile.lock to avoid potential errors on Mac.

--- manifests/profile/www_lib/php.pp
+++ manifests/profile/www_lib/php.pp
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 The Regents of the University of Michigan.
+# Copyright (c) 2019-2020 The Regents of the University of Michigan.
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
 
@@ -8,10 +8,65 @@
 #
 # @example
 #   include nebula::profile::www_lib::php
-class nebula::profile::www_lib::php () {
-  include nebula::profile::php73
+class nebula::profile::www_lib::php (
+  String $default_php_version = '7.3'
+) {
 
-  package {
+  # Set the php repo
+  apt::source { 'php-community':
+    location     => 'https://packages.sury.org/php/',
+    key          =>  {
+      id     => '15058500A0235D97F5D10063B188E2B695BD4743',
+      source => 'https://packages.sury.org/php/apt.gpg'
+    },
+    release      => $::lsbdistcodename,
+    repos        => 'main',
+    architecture => $::os['architecture'],
+  }
+
+  # Set default php
+  class { '::php::globals':
+    php_version => $default_php_version,
+    config_root => "/etc/php/${default_php_version}",
+  }
+
+  # Install default php packages. Some are implicit as described 
+  # below while rest have to be specified.
+  #
+  # php*-cli, php*-common, php*-fpm and php-pear get installed by 
+  # the puppet PHP module by default (if enabled). Devel packages 
+  # are implicit also but we aren't using them.
+  #
+  # Note: The PHP module doesn't use ensure_packages so if we don't
+  # remove them below we will get duplicate warnings despite our
+  # use of ensure_packages.
+  #
+  ensure_packages (
+    [
+      'php-common',
+      'php-geoip',
+      'php-igbinary',
+      'php-imagick',
+      'php-memcached',
+      'php-msgpack',
+      'php-redis',
+      'php-xdebug',
+      'php-xml',
+      "php${default_php_version}-curl",
+      "php${default_php_version}-gd",
+      "php${default_php_version}-json",
+      "php${default_php_version}-ldap",
+      "php${default_php_version}-mbstring",
+      "php${default_php_version}-mysql",
+      "php${default_php_version}-opcache",
+      "php${default_php_version}-readline",
+      "php${default_php_version}-sqlite3",
+      "php${default_php_version}-xml",
+    ]
+  )
+
+  # Install php 5.6
+  ensure_packages (
     [
       'php5.6-cli',
       'php5.6-common',
@@ -26,23 +81,44 @@ class nebula::profile::www_lib::php () {
       'php5.6-readline',
       'php5.6-sqlite3',
       'php5.6-xml',
-    ]:
-  }
+    ]
+  )
 
-  package {
+  # Pear and database packages
+  ensure_packages (
     [
       'php-mdb2',
       'php-mdb2-driver-mysql'
-    ]:
+    ]
+  )
+
+  # Configure FPM config file
+  php::config { 'fpm php.ini':
+    file   => "/etc/php/${default_php_version}/fpm/php.ini",
+    config => {
+      'PHP/short_open_tag'          => 'On',
+      'PHP/max_input_vars'          => '2000',
+      'PHP/memory_limit'            => '256M',
+      'PHP/error_reporting'         => 'E_ALL & ~E_DEPRECATED',
+      'PHP/upload_max_filesize'     => '64M',
+      'Date/date.timezone'          => 'America/Detroit',
+      'mail function/sendmail_path' => '/usr/sbin/sendmail -t -i',
+    }
   }
 
+  # Configure default PHP
   class { '::php':
-    ensure       => present,     # Don't touch stuff from above; should be equivalent
+    ensure       => present, # Don't touch stuff from above; should be equivalent
     manage_repos => false, # Set true to add dotdeb repos
     fpm          => true,
-    composer     => false,     # System-wide composer seems iffy unless using dotdeb
-    pear         => true,          # We're using this for PEAR, so set to true
-    phpunit      => true,       # Unsure whether this should be system or app-level
+    fpm_user     => 'nobody',
+    fpm_group    => 'nogroup',
+    composer     => false, # System-wide composer seems iffy unless using dotdeb
+    pear         => true,  # We're using this for PEAR, so set to true
+    phpunit      => true,  # Unsure whether this should be system or app-level
+
+    # Disable default 'www' pool creation; it is created below manually.
+    fpm_pools    => {},
 
     # Some of these may only be needed for mirlyn, so if/when the mirlyn API is
     # removed, we should be able to remove these
@@ -76,6 +152,42 @@ class nebula::profile::www_lib::php () {
     },
   }
 
+  # Configure FPM default pool ('www')
+  # 
+  # The 'www' pool is hard-coded in the php module so can't be created here.
+  #
+  # The php::fpm::pool class is intended to be used to create other pools only.
+  #
+  # Options to circumvent the php module design is to either:
+  #   1. Adjust the ::php { fpm_pools => {settings} } here.
+  #   2. Adjust the settings in hiera as:
+  #     - php::params::fpm_tools:
+  #         www:
+  #   3. Set fpm_pools => {} to disable default 'www' creation and then 
+  #      create it manually using php::fpm::pool class like any pool. This 
+  #      is the option we are choosing.
+  #
+  #
+  php::fpm::pool { 'www':
+    user                      => 'nobody',
+    group                     => 'nogroup',
+    listen                    => "/run/php/php${default_php_version}-fpm.sock",
+    listen_owner              => 'nobody',
+    listen_group              => 'nogroup',
+    pm                        => 'dynamic',
+    pm_max_children           => 5,
+    pm_start_servers          => 2,
+    pm_min_spare_servers      => 1,
+    pm_max_spare_servers      => 3,
+
+    # Default PHP puppet module settings from fpm_pools
+    catch_workers_output      => 'no',
+    pm_max_requests           => 0,
+    request_terminate_timeout => 0,
+  }
+
+
+  # Configure Apache PHP module
   class { 'php::apache_config':
     inifile  => '/etc/php/5.6/apache2/php.ini',
     settings => {
@@ -88,5 +200,4 @@ class nebula::profile::www_lib::php () {
       'mail function/sendmail_path' => '/usr/sbin/sendmail -t -i'
     },
   }
-
 }
