f3ed50d657f7ede8d595ce8f79c77fcb9b994719
Suleman Diwan <suledwan@umich.edu>
2020-06-09 12:35:27 -0400

    Remove update.lib

--- manifests/profile/www_lib/vhosts/redirects.pp
+++ manifests/profile/www_lib/vhosts/redirects.pp
@@ -179,16 +179,6 @@ class nebula::profile::www_lib::vhosts::redirects(
     target        => 'http://www.lib.umich.edu/health-sciences-libraries/medsearch/'
   }
 
-  nebula::apache::redirect_vhost_http { 'm-update.lib.umich.edu':
-    serveraliases => ['m-update.lib'],
-    target        => 'http://m.lib.umich.edu/'
-  }
-
-  nebula::apache::redirect_vhost_http { 'www-update.lib.umich.edu':
-    serveraliases => ['www-update.lib'],
-    target        => 'http://www.lib.umich.edu/'
-  }
-
   nebula::apache::redirect_vhost_http { 'pk.lib.umich.edu':
     target => 'http://www.lib.umich.edu/pk/'
   }
