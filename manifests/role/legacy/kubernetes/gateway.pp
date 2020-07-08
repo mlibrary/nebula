a9a6bbfb97dabeda7d6f1d969136386b40d23465
Matthew Alexander LaChance <mattlach@umich.edu>
2020-07-08 07:53:10 -0400

    Remove legacy kubernetes profiles
    They're no longer in use

--- manifests/role/legacy/kubernetes/gateway.pp
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2019 The Regents of the University of Michigan.
-# All Rights Reserved. Licensed according to the terms of the Revised
-# BSD License. See LICENSE.txt for details.
-
-# Kubernetes gateway node
-class nebula::role::legacy::kubernetes::gateway {
-  include nebula::role::minimum
-  include nebula::profile::legacy::kubernetes::haproxy
-}
