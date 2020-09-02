9a7874c6b808c64219e0f501d981fec386738dd8
jnega <jnega@umich.edu>
2020-09-02 13:04:08 -0400

    final addition of git repo for yarn

--- manifests/profile/yarn.pp
+++ manifests/profile/yarn.pp
@@ -11,14 +11,7 @@ class nebula::profile::yarn (
   apt::source { 'yarn':
     location     => 'https://dl.yarnpkg.com/debian/',
     key          =>  {
-<<<<<<< HEAD
-<<<<<<< HEAD
       id     => 'B90F6449FEBC20F00DB13ED8212659B22565CA86',
-=======
->>>>>>> 4c5e426... added info for yarn repo
-=======
-      id     => 'B90F6449FEBC20F00DB13ED8212659B22565CA86',
->>>>>>> 9c77a2e... added apt key id
       source => 'https://dl.yarnpkg.com/debian/pubkey.gpg'
     },
     release      => 'stable',
@@ -26,26 +19,8 @@ class nebula::profile::yarn (
     architecture => $::os['architecture'],
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   package { 'yarn':
     require => Apt::Source['yarn'],
   }
-=======
-  package { ['yarn']: }
->>>>>>> 4c5e426... added info for yarn repo
-=======
-  package { ['yarn'] }
->>>>>>> a00ec19... syntax fix
-=======
-  package { ['yarn']: }
->>>>>>> 2319f52... revert syntax fix
-=======
-  package { 'yarn':
-    require => Apt::Source['yarn'],
-  }
->>>>>>> a92c049... added requirment for install after repo is set up
 
 }
