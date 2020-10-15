8f1ad6f8177c8b1289cd7619ebf3d7fd3a5712dd
Matthew Alexander LaChance <mattlach@umich.edu>
2020-10-15 09:00:11 -0400

    AEIM-3113 - Create aleph role with a RedHat 6 test harness

---
+++ spec/classes/redhat6/role/aleph_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2020 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+require 'spec_helper'
+
+describe 'nebula::redhat6::role::aleph' do
+  test_on = {
+    supported_os: [
+      {
+        'operatingsystem' => 'RedHat',
+        'operatingsystemrelease' => ['6'],
+      },
+    ],
+  }
+
+  on_supported_os(test_on).each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+    end
+  end
+end
