5d01558b09dc457c2e6f70bccb6225fb1612ba6b
Matthew Alexander LaChance <mattlach@umich.edu>
2021-01-20 16:15:42 -0500

    Add explicit symlinks for http fileserver

--- manifests/profile/http_fileserver.pp
+++ manifests/profile/http_fileserver.pp
@@ -45,4 +45,30 @@ class nebula::profile::http_fileserver (
 
   include nebula::profile::networking::firewall::http_datacenters
 
+  file {
+    default:
+      ensure => 'link',
+    ;
+
+    '/etc/apache2/conf-enabled/charset.conf':
+      target => '../conf-available/charset.conf',
+    ;
+
+    '/etc/apache2/conf-enabled/localized-error-pages.conf':
+      target => '../conf-available/localized-error-pages.conf',
+    ;
+
+    '/etc/apache2/conf-enabled/other-vhosts-access-log.conf':
+      target => '../conf-available/other-vhosts-access-log.conf',
+    ;
+
+    '/etc/apache2/conf-enabled/security.conf':
+      target => '../conf-available/security.conf',
+    ;
+
+    '/etc/apache2/conf-enabled/serve-cgi-bin.conf':
+      target => '../conf-available/serve-cgi-bin.conf',
+    ;
+  }
+
 }
