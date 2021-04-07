4e7907257698ad869b1c8a796f18fc18d19b5db4
Noah Botimer <botimer@umich.edu>
2021-04-06 21:27:15 -0400

    SYS-171 - Remove CPAN modules that are duplicated by debs
    It turns out a bunch of these test modules are in debs, so the hack is
    not needed. No shame in our profiles getting shorter and simpler, rather
    than longer with more edge cases.

--- manifests/cpan.pp
+++ manifests/cpan.pp
@@ -9,16 +9,8 @@
 # @example
 #   nebula::cpan { 'Test::More': }
 define nebula::cpan {
-  # Test::NoWarnings runs an extra test at exit by default, which fails itself
-  # out unless you have an actual test suite, so we have to tell it to skip it.
-  if $title == 'Test::NoWarnings' {
-    $expression = "$Test::NoWarnings::do_end_test = 0"
-  } else {
-    $expression = "1"
-  }
-
   exec { "CPAN - ${title}":
     command => "/usr/bin/cpan -i ${title}",
-    unless  => "/usr/bin/env perl -M${title} -e '${expression}' >/dev/null 2>&1"
+    unless  => "/usr/bin/env perl -M${title} -e1 >/dev/null 2>&1",
   }
 }
