492d171b0de8049ac94ed6f3920cdc2a13d16e03
Matthew Alexander LaChance <mattlach@umich.edu>
2021-04-28 13:49:21 -0400

    Remove backup keepalived config for now

--- manifests/profile/keepalived/backup.pp
+++ manifests/profile/keepalived/backup.pp
@@ -3,19 +3,4 @@
 # BSD License. See LICENSE.txt for details.
 
 class nebula::profile::keepalived::backup {
-  include keepalived
-
-  $scope = lookup('nebula::scope')
-
-  lookup('nebula::profile::keepalived::ip_addresses', undef, undef, []).each |$index, $ip_address| {
-    $id = $index + 50
-
-    keepalived::vrrp::instance { "${::datacenter}_${scope}_${id}":
-      state             => 'BACKUP',
-      priority          => 100,
-      interface         => pick_network_interface_for_ip($ip_address),
-      virtual_router_id => $id,
-      virtual_ipaddress => $ip_address,
-    }
-  }
 }
