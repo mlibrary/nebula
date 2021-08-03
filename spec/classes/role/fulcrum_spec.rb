d0ee635076a4a95af27382d29be674b0b8eecc17
Jody Nega <jnega@umich.edu>
2021-08-03 12:05:48 -0400

    add new spec file

---
+++ spec/classes/role/fulcrum_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2021 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+require 'spec_helper'
+
+describe 'nebula::role::fulcrum' do
+  it { is_expected.to compile }
+end
