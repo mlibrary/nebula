15615ceab0c72de5f8cd9497f16bd504c78657af
Noah Botimer <botimer@umich.edu>
2021-08-19 13:02:59 -0400

    Attempt minimal configuration for SSL ACME challenges

---
+++ manifests/profile/fulcrum/apache.pp
@@ -0,0 +1,24 @@
+# Copyright (c) 2021 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+
+# nebula::profile::fulcrum::apache
+#
+# Apache web server for hosting a Fulcrum server.
+#
+# This profile sets up Apache to serve the static assets and reverse proxy to
+# the application via HTTPS. It also sets up the default virtual host to handle
+# HTTP ACME challenges for Let's Encrypt and Certbot. We do not use the
+# ssl_keypair profile because of this.
+
+class nebula::profile::fulcrum::apache (
+  String $servername = $::fqdn
+) {
+  include nebula::profile::letsencrypt
+  class { 'apache': }
+
+  letsencrypt::certonly { "Certificate: ${servername}":
+    domains => [$servername],
+    plugin  => 'apache',
+  }
+}
---
+++ spec/classes/profile/fulcrum/apache_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2021 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+require 'spec_helper'
+
+describe 'nebula::profile::fulcrum::apache' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+
+      it { is_expected.to contain_class('letsencrypt') }
+      it { is_expected.to contain_class('apache') }
+    end
+  end
+end
