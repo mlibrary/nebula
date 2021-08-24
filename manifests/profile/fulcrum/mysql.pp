f7a0af709269e5926eaea6e5ab06d61e18bf5400
Noah Botimer <botimer@umich.edu>
2021-08-24 19:05:44 -0400

    Set up Fedora under Tomcat user instance
    - Manage fulcrum user and group
    - Manage repository directory
    - Add Tomcat user instance in fulcrum home directory
    - Download Fedora .war file into webapps directory
    - Create app and fedora databases
    - Add repository.json for modeshape config
    - Add defaults file for Tomcat environment
    - Add service unit to launch Tomcat

---
+++ manifests/profile/fulcrum/mysql.pp
@@ -0,0 +1,24 @@
+# Copyright (c) 2021 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+
+# nebula::profile::fulcrum::mysql
+
+class nebula::profile::fulcrum::mysql (
+  String $fedora_password,
+  String $fulcrum_password,
+) {
+  include nebula::profile::mysql
+
+  mysql::db { 'fedora':
+    user     => 'fedora',
+    password => $fedora_password,
+    host     => 'localhost',
+  }
+
+  mysql::db { 'fulcrum':
+    user     => 'fulcrum',
+    password => $fulcrum_password,
+    host     => 'localhost',
+  }
+}
