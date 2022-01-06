59c3ca5f8312ea4633edcb7f04577af52c0d3cf3
Matthew Alexander LaChance <mattlach@umich.edu>
2022-01-06 08:48:52 -0500

    Add log rotation to fulcrum.
    These settings are copied right from bulleit-1 for heliotrope-production
    logging.

---
+++ spec/classes/profile/fulcrum/logrotate_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2022 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+require 'spec_helper'
+
+describe 'nebula::profile::fulcrum::logrotate' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+
+      it { is_expected.to contain_class('Nebula::Profile::Logrotate') }
+
+      it do
+        is_expected.to contain_logrotate__rule('fulcrum')
+          .with_path('/home/fulcrum/app/shared/log/*.log')
+          .with_rotate(7)
+          .with_rotate_every('day')
+          .with_missingok(true)
+          .with_compress(true)
+          .with_ifempty(false)
+          .with_delaycompress(true)
+          .with_copytruncate(true)
+      end
+    end
+  end
+end
