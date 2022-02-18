04db13eecd956c9060f92be58a9c7c423dd090a6
Matthew Alexander LaChance <mattlach@umich.edu>
2022-02-18 09:40:44 -0500

    WIP alma integrations
    My intention was to separate this into two profiles: one for an sftp
    server, and another for using logrotate to automatically remove old
    files.

---
+++ manifests/profile/alma_integrations.pp
@@ -0,0 +1,11 @@
+# Copyright (c) 2022 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+
+class nebula::profile::alma_integrations {
+  user { 'alma':
+    home => '/var/lib/alma'
+  }
+
+  file { '
+}
