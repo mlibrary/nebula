1801e8140d7440986f345688e83c40fe96f89184
Matthew Alexander LaChance <mattlach@umich.edu>
2022-04-19 10:06:40 -0400

    Add bastion role and bolt profile

---
+++ manifests/profile/bolt.pp
@@ -0,0 +1,7 @@
+# Copyright (c) 2022 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+
+class nebula::profile::bolt {
+  package { 'puppet-bolt': }
+}
