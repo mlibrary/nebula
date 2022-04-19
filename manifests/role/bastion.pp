7ffea440f77908374503f802bcc2f968fcdae98f
Matthew Alexander LaChance <mattlach@umich.edu>
2022-04-19 18:42:36 -0400

    Add comment explaining why these three profiles

--- manifests/role/bastion.pp
+++ manifests/role/bastion.pp
@@ -5,8 +5,11 @@
 class nebula::role::bastion {
   include nebula::role::minimum
   include nebula::profile::bolt
+  include nebula::profile::root_ssh_private_keys
+
+  # These three are effectively the requirements for getting user login
+  # with kerberos and duo.
   include nebula::profile::duo
   include nebula::profile::krb5
   include nebula::profile::networking
-  include nebula::profile::root_ssh_private_keys
 }
