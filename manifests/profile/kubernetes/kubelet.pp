ba08dfdea05f6dbed5e8a79e441be64924c628cf
Matthew Alexander LaChance <mattlach@umich.edu>
2022-07-21 16:17:59 -0400

    Fix cgroup hierarchy for containerd in bullseye

--- manifests/profile/kubernetes/kubelet.pp
+++ manifests/profile/kubernetes/kubelet.pp
@@ -37,6 +37,13 @@ class nebula::profile::kubernetes::kubelet {
 
   package { 'containerd': }
   kmod::load { 'br_netfilter': }
+  file { '/etc/default/grub.d/cgroup.cfg':
+    contents => "GRUB_CMDLINE_LINUX=systemd.unified_cgroup_hierarchy=false\n",
+    notify   => Exec['/usr/sbin/update-grub']
+  }
+  exec { '/usr/sbin/update-grub':
+    refreshonly => true,
+  }
 
   service { 'kubelet':
     ensure  => 'running',
