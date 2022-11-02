01bda65d0aed0b28b41de5c1fd11a3eab1e190a4
Ryan Rotter <rrotter@users.noreply.github.com>
2022-11-02 15:59:57 -0500

    Generate etcd yaml with erb
    Config is written to /tmp on etcd nodes, iff etcd_initial_cluster
    var is defined for the present cluster.

--- manifests/profile/kubernetes/bootstrap/etcd_config.pp
+++ manifests/profile/kubernetes/bootstrap/etcd_config.pp
@@ -1,9 +1,15 @@
-# Copyright (c) 2020 The Regents of the University of Michigan.
+# Copyright (c) 2020, 2022 The Regents of the University of Michigan.
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
 
 class nebula::profile::kubernetes::bootstrap::etcd_config {
   include nebula::profile::kubernetes::kubelet
+  $cluster_name = lookup('nebula::profile::kubernetes::cluster')
+  $cluster = lookup('nebula::profile::kubernetes::clusters')[$cluster_name]
+
+  $router_address = $cluster['router_address']
+  $private_domain = $cluster['private_domain']
+  $initial_cluster = $cluster['etcd_initial_cluster']
 
   file { '/etc/systemd/system/kubelet.service.d/20-etcd-service-manager.conf':
     content => template('nebula/profile/kubernetes/bootstrap/etcd/systemd.conf.erb'),
@@ -13,6 +19,13 @@ class nebula::profile::kubernetes::bootstrap::etcd_config {
 
   file { '/etc/systemd/system/kubelet.service.d':
     ensure => 'directory',
+  }
+
+  if $initial_cluster {
+    file { '/tmp/etcd.yaml':
+      ensure => 'file',
+      content => template('nebula/profile/kubernetes/etcd/etcd.yaml.erb'),
+    }
   }
 
   exec { 'kubelet reload daemon':
