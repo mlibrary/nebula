59a3e200a4f8e6d2de179ebafdf1e9c9b7780bc3
Jody Nega <jnega@umich.edu>
2023-03-17 16:30:26 -0400

    WIP add exec to set and maintain default JDK

--- manifests/profile/openjdk_java.pp
+++ manifests/profile/openjdk_java.pp
@@ -6,7 +6,15 @@
 #
 class nebula::profile::openjdk_java (
   Array[String] $jdk_packages = ['openjdk-11-jdk-headless']
+  String $default_jdk = 'openjdk-11-jdk-headless',
+  String $base_alternative = 'java-11-openjdk-amd64',
+  String $java_alternative = 'java-1.11.0-openjdk-amd64'
 ) {
   ensure_packages($jdk_packages)
+  exec { 'ensure default java':
+    command => "/usr/sbin/update-java-alternatives -s ${java_alternative}",
+    unless => "/usr/bin/update-alternatives --query java | grep '^Value:.*${base_alternative}'",
+    require => Package[$default_jdk]
+  }
 }
 
