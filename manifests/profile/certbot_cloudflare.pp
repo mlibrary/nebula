2674683b60c7755ce8347655523db15c58cfbf5b
Ryan Rotter <rrotter@users.noreply.github.com>
2023-08-09 14:46:02 -0400

    add support for certbot cloudflare plugin

---
+++ manifests/profile/certbot_cloudflare.pp
@@ -0,0 +1,72 @@
+# Copyright (c) 2022-2023 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+
+class nebula::profile::certbot_cloudflare (
+  Hash[String, Hash[String, Array[String]]] $certs = {},
+  String $cert_dir = "/var/local/cert_dir",
+  String $haproxy_cert_dir = "/var/local/haproxy_cert_dir",
+  String $letsencrypt_email = "nope@nope.zone",
+  String $cloudflare_api_token = "default.invalid",
+) {
+  ensure_packages([
+    "certbot",
+    "python3-certbot-dns-cloudflare",
+  ])
+
+  file { "/root/.secrets":
+    ensure => "directory"
+  }
+
+  file { "/root/.secrets/certbot":
+    ensure  => "directory",
+    mode    => "0700"
+  }
+
+  file { "/root/.secrets/certbot/cloudflare.ini":
+    mode    => "0600",
+    content => template("nebula/profile/certbot_cloudflare/cloudflare.ini.erb")
+  }
+
+  file { "/tmp/all_cert_commands_cloudflare":
+    content => template("nebula/profile/certbot_cloudflare/commands.erb")
+  }
+
+  $certs.each |$service, $domains| {
+    $domains.each |$main_domain, $alt_domains| {
+      concat { "${cert_dir}/${main_domain}.crt":
+        group  => "puppet",
+      }
+
+      concat { "${cert_dir}/${main_domain}.key":
+        group  => "puppet",
+      }
+
+      concat { "${haproxy_cert_dir}/${service}/${main_domain}.pem":
+        group => "puppet",
+      }
+
+      concat_fragment { "${main_domain}.crt cert":
+        target => "${cert_dir}/${main_domain}.crt",
+        source => "/etc/letsencrypt/live/${main_domain}/fullchain.pem"
+      }
+
+      concat_fragment { "${main_domain}.key key":
+        target => "${cert_dir}/${main_domain}.key",
+        source => "/etc/letsencrypt/live/${main_domain}/privkey.pem"
+      }
+
+      concat_fragment { "${main_domain}.pem cert":
+        order  => "01",
+        target => "${haproxy_cert_dir}/${service}/${main_domain}.pem",
+        source => "/etc/letsencrypt/live/${main_domain}/fullchain.pem"
+      }
+
+      concat_fragment { "${main_domain}.pem key":
+        order  => "02",
+        target => "${haproxy_cert_dir}/${service}/${main_domain}.pem",
+        source => "/etc/letsencrypt/live/${main_domain}/privkey.pem"
+      }
+    }
+  }
+}
