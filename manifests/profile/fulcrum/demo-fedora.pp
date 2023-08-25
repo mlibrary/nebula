d8a82381703f14e2302d7ba5da9e42a68eacec3f
Anthony Thomas <antmoth@umich.edu>
2023-08-25 11:30:57 -0400

    fix demo-fedora name

--- manifests/profile/fulcrum/demo-fedora.pp
+++ manifests/profile/fulcrum/demo-fedora.pp
@@ -2,9 +2,9 @@
 # All Rights Reserved. Licensed according to the terms of the Revised
 # BSD License. See LICENSE.txt for details.
 
-# nebula::profile::fulcrum::fedora
+# nebula::profile::fulcrum::demo-fedora
 
-class nebula::profile::fulcrum::fedora (
+class nebula::profile::fulcrum::demo-fedora (
   String $fedora_username = 'fedora',
   String $fedora_password = lookup('nebula::profile::fulcrum::mysql::fedora_password'),
 ) {
