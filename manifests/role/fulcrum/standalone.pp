0727c0922a64556ebc5b7340d70f96828fca59c1
Anthony Thomas <antmoth@umich.edu>
2023-09-13 09:40:54 -0400

    add fulcrum::nginx back in

--- manifests/role/fulcrum/standalone.pp
+++ manifests/role/fulcrum/standalone.pp
@@ -13,9 +13,9 @@ class nebula::role::fulcrum::standalone {
   include nebula::profile::fulcrum::hosts
   include nebula::profile::fulcrum::app
   include nebula::profile::fulcrum::logrotate
+  include nebula::profile::fulcrum::nginx
 # include nebula::profile::fulcrum::mysql
 # include nebula::profile::fulcrum::fedora
-# include nebula::profile::fulcrum::nginx
 # include nebula::profile::fulcrum::redis
 # include nebula::profile::fulcrum::shibboleth
 # include nebula::profile::fulcrum::solr
