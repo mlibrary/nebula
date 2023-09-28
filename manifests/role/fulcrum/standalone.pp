3e7207aed57ddbd5b826042bcffdf74c242b9bd0
Anthony Thomas <antmoth@umich.edu>
2023-09-28 02:24:09 -0400

    add rest of apache stuff

--- manifests/role/fulcrum/standalone.pp
+++ manifests/role/fulcrum/standalone.pp
@@ -7,9 +7,9 @@
 # This is desiged to manage a Debian Server that hosts the Fulcrum project, with all of the dependencies and services included. 
 
 class nebula::role::fulcrum::standalone (
-# String $private_address_template = '192.168.0.%s',
-# String $shibboleth_config_source = 'puppet:///shibboleth-fulcrum',
-# Hash $hosts = {}
+  String $private_address_template = '192.168.0.%s',
+  String $shibboleth_config_source = 'puppet:///shibboleth-fulcrum',
+  Hash $hosts = {}
 ) {
 
   include nebula::role::minimum
@@ -19,45 +19,45 @@ class nebula::role::fulcrum::standalone (
   include nebula::profile::fulcrum::app
   include nebula::profile::fulcrum::logrotate
   include nebula::profile::fulcrum::redis
-# include nebula::profile::fulcrum::solr
-# include nebula::profile::fulcrum::mysql
 
   # The perl profile is needed for monitor_pl to work, but it pulls in a
   # ton of stuff. We should probably allow for different haproxy http checks
   # for a service, and eliminate the perl/monitor_pl dependency here.
   include nebula::profile::www_lib::perl
 
-# create_resources('host',$hosts)
+  create_resources('host',$hosts)
 
   include nebula::profile::www_lib::apache::base
   include nebula::profile::www_lib::apache::fulcrum
 
-# class { 'nebula::profile::shibboleth':
-#   config_source    => $shibboleth_config_source,
-#   watchdog_minutes => '*/30',
-# }
+  class { 'nebula::profile::shibboleth':
+    config_source    => $shibboleth_config_source,
+    watchdog_minutes => '*/30',
+  }
 
-# cron {
-#   default:
-#     user => 'root',
-#   ;
+  cron {
+    default:
+      user => 'root',
+    ;
 
-#   'purge apache access logs 1/2':
-#     hour    => 1,
-#     minute  => 7,
-#     command => '/usr/bin/find /var/log/apache2 -type f -mtime +14 -name "*log*" -exec /bin/rm {} \; > /dev/null 2>&1',
-#   ;
+    'purge apache access logs 1/2':
+      hour    => 1,
+      minute  => 7,
+      command => '/usr/bin/find /var/log/apache2 -type f -mtime +14 -name "*log*" -exec /bin/rm {} \; > /dev/null 2>&1',
+    ;
 
-#   'purge apache access logs 2/2':
-#     hour    => 1,
-#     minute  => 17,
-#     command => '/usr/bin/find /var/log/apache2 -type f -mtime +2  -name "*log*" ! -name "*log*gz" -exec /usr/bin/pigz {} \; > /dev/null 2>&1',
-#     require => Package['pigz'],
-#   ;
-# }
+    'purge apache access logs 2/2':
+      hour    => 1,
+      minute  => 17,
+      command => '/usr/bin/find /var/log/apache2 -type f -mtime +2  -name "*log*" ! -name "*log*gz" -exec /usr/bin/pigz {} \; > /dev/null 2>&1',
+      require => Package['pigz'],
+    ;
+  }
 
-# ensure_packages(['pigz'])
+  ensure_packages(['pigz'])
 
+# include nebula::profile::fulcrum::solr
+# include nebula::profile::fulcrum::mysql
 # include nebula::profile::fulcrum::shibboleth
 # include nebula::profile::fulcrum::fedora
 }
