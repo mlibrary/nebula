ceca98c871f3767c508d798b4a79479736e25a6f
Anthony Thomas <antmoth@umich.edu>
2023-09-28 01:56:48 -0400

    add things back in to standalone role

--- manifests/role/fulcrum/standalone.pp
+++ manifests/role/fulcrum/standalone.pp
@@ -13,12 +13,12 @@ class nebula::role::fulcrum::standalone (
 ) {
 
   include nebula::role::minimum
-# include nebula::profile::ruby
-# include nebula::profile::fulcrum::base
-# include nebula::profile::fulcrum::hosts
-# include nebula::profile::fulcrum::app
-# include nebula::profile::fulcrum::logrotate
-# include nebula::profile::fulcrum::redis
+  include nebula::profile::ruby
+  include nebula::profile::fulcrum::base
+  include nebula::profile::fulcrum::hosts
+  include nebula::profile::fulcrum::app
+  include nebula::profile::fulcrum::logrotate
+  include nebula::profile::fulcrum::redis
 # include nebula::profile::fulcrum::solr
 # include nebula::profile::fulcrum::mysql
 
