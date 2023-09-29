d1f12f8bd1ae8f69304fedf5d5c861c79d391fd3
Anthony Thomas <antmoth@umich.edu>
2023-09-29 16:49:46 -0400

    missed one

--- manifests/profile/fulcrum/shibboleth.pp
+++ manifests/profile/fulcrum/shibboleth.pp
@@ -52,7 +52,7 @@ class nebula::profile::fulcrum::shibboleth {
     ensure     => 'running',
     enable     => true,
     hasrestart => true,
-    require    => [Package['shibboleth-sp2-utils'], Package['mariadb-unixodbc']]
+    require    => [Package['shibboleth-sp-utils'], Package['mariadb-unixodbc']]
   }
 
   service { 'shibauthorizer.socket':
