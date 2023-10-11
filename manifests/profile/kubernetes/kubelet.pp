7d10d2df3323b019f9fa1cc330c2b8a64b21595e
Ryan Rotter <rrotter@users.noreply.github.com>
2023-10-11 13:51:38 -0400

    enable nftables for kubelet

--- manifests/profile/kubernetes/kubelet.pp
+++ manifests/profile/kubernetes/kubelet.pp
@@ -98,4 +98,16 @@ class nebula::profile::kubernetes::kubelet {
       dport => 5473,
     ;
   }
+
+  file { "/etc/modules-load.d/kubernetes.conf":
+    content => "br_netfilter
+    ",
+  }
+  file { "/etc/sysctl.d/kubernetes.conf":
+    content => "net.bridge.bridge-nf-call-ip6tables = 1
+    net.bridge.bridge-nf-call-iptables = 1
+    net.ipv4.ip_forward = 1
+    ",
+  }
+
 }
