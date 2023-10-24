954fa0d19479384bea98191fed27e03bade15caa
Anthony Thomas <antmoth@umich.edu>
2023-10-24 12:08:39 -0400

    Basic authz-test role and profile

---
+++ manifests/profile/authz_test.pp
@@ -0,0 +1,13 @@
+# Copyright (c) 2023 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+
+class nebula::profile::authz_test {
+  include nebula::profile::apache
+  include nebula::profile::apache::authz_umichlib
+
+  ensure_packages( ['libjson-xs-perl'] )
+  nebula::cpan { [ 'CGI'] }
+  Package <| |> -> Nebula::Cpan <| |>
+
+}
