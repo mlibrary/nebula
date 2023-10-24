9b4730c38dfa0bd057dd4e48acb82d73a95a6f1d
Anthony Thomas <antmoth@umich.edu>
2023-10-24 14:55:52 -0400

    add some missing apache mods

--- manifests/profile/authz_test.pp
+++ manifests/profile/authz_test.pp
@@ -3,12 +3,24 @@
 # BSD License. See LICENSE.txt for details.
 
 class nebula::profile::authz_test {
+  include nebula::profile::logrotate
+
   include nebula::profile::apache
   include nebula::profile::apache::authz_umichlib
 
+  include apache::mod::auth_basic
+  include apache::mod::authn_core
+  include apache::mod::authn_file
+  include apache::mod::authz_user
+  include apache::mod::cgi
+  include apache::mod::env
+  include apache::mod::headers
+  include apache::mod::include
+  include apache::mod::setenvif
+
   ensure_packages([
     'build-essential',
-    'libjson-xs-perl'
+    'libjson-xs-perl',
   ])
   nebula::cpan { ['CGI']: }
   Package <| |> -> Nebula::Cpan <| |>
