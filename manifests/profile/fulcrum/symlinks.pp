cfae0f1029928fd815b5ee708a0a207dfac5894d
Anthony Thomas <antmoth@umich.edu>
2023-12-08 00:30:41 -0500

    try adding print debugging to symlinks

--- manifests/profile/fulcrum/symlinks.pp
+++ manifests/profile/fulcrum/symlinks.pp
@@ -8,6 +8,7 @@ class nebula::profile::fulcrum::symlinks (
   Hash $config = {},
 ) {
   $config.each |$link, $target| {
+    notify { "building symlink ${$link} to ${$target}": }
     file { $link:
       ensure => 'link',
       target => $target,
