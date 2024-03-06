2924fd9906ecd2e208e5d4291f4ab0d89c80cfd1
Ryan Rotter <rrotter@users.noreply.github.com>
2024-03-06 17:30:55 -0500

    add tests confirming existing resolv_conf behavior
    - resolv_conf sets ensure absent on the resolvconf package
      - It's not clear why, but we should not change this behavior until it is understood
    - test that /etc/resolv.conf is created correctly

--- spec/classes/profile/dns/standard_spec.rb
+++ spec/classes/profile/dns/standard_spec.rb
@@ -15,6 +15,17 @@ describe 'nebula::profile::dns::standard' do
           ['5.5.5.5', '4.4.4.4'],
         ).with_searchpath(['searchpath.default.invalid'])
       end
+
+      it 'removes resolvconf package if present' do
+        is_expected.to contain_package('resolvconf').with_ensure('absent')
+      end
+      it 'contains expected resolv.conf file' do
+        is_expected.to contain_file('/etc/resolv.conf')
+          .with_content(/^#.*puppet/)
+          .with_content(/^search searchpath\.default\.invalid$/)
+          .with_content(/^nameserver 5.5.5.5$/)
+          .with_content(/^nameserver 4.4.4.4$/)
+      end
     end
   end
 end
