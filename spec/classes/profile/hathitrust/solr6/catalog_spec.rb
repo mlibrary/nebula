7268a2df891331a0af475d3202de1eacbd0e8675
Ryan Rotter <rrotter@users.noreply.github.com>
2024-03-20 16:49:42 -0400

    index-release.sh.erb refactor

--- spec/classes/profile/hathitrust/solr6/catalog_spec.rb
+++ spec/classes/profile/hathitrust/solr6/catalog_spec.rb
@@ -13,17 +13,15 @@ describe 'nebula::profile::hathitrust::solr6::catalog' do
       it { is_expected.to contain_file('/usr/local/bin/index-release')
         .with_content(%r|BASE=/htsolr/catalog|)
         .with_content(%r|^SNAP=".snapshot/htsolr-catalog_\${TODAY}"$|)
-        .with_content(%r|CORES="catalog"|)
+        .with_content(/CORES="catalog"/)
         .with_content(%r|/flags/STOPCATALOGRELEASE|)
         .with_content(%r|/bin/echo "STOPCATALOGRELEASE flag present|)
-        .with_content(%r|curl -A SOLR -s --retry 5 --fail https://babel.hathitrust.org/flags/web/catalog-release-|)
-        .with_content(%r{babel.hathitrust.org:443:6.5.4.3})
         .with_content(%r|SEGMENTS=`ls \${BASE}/cores/\${s}/\${SNAP}/data/index/|)
-           .with_content(%r|if \[ \${SEGMENTS} -eq 0 \];|)
-        .without_content(%r(if \[ \${SEGMENTS} -lt 1 \] \|\| \[ \${SEGMENTS} -gt 2 \];))
+           .with_content(/if \[ \${SEGMENTS} -eq 0 \];/)
+        .without_content(/if \[ \${SEGMENTS} -lt 1 \] \|\| \[ \${SEGMENTS} -gt 2 \];/)
         .with_content(%r|rm -f \${SYMLINKBASE}/\${s} && ln -s \${BASE}/cores/\${s}/\${SNAP} \${SYMLINKBASE}/\${s}$|)
         .with_content(/^# run the first query to initialize catalog solr$/)
-        .with_content(/^## run the first query to initialize lss solr$/)
+        .without_content(/^# run the first query to initialize lss solr$/)
       }
       it { is_expected.to contain_cron('catalog solr index release')
         .with(command: "/usr/local/bin/index-release > /tmp/index-release.log 2>&1 || /usr/bin/mail -s 'foo catalog index release problem' anybody@default.invalid < /tmp/index-release.log")
@@ -33,7 +31,8 @@ describe 'nebula::profile::hathitrust::solr6::catalog' do
         let(:params) do
           { is_primary_site: true }
         end
-        it { is_expected.to contain_file('/usr/local/bin/index-release').with_content(%r{^if ! curl -A SOLR -s --retry 5 --fail https://babel.hathitrust.org}) }
+        it { is_expected.to contain_file('/usr/local/bin/index-release')
+          .with_content(%r|^if ! curl -A SOLR -s --retry 5 --fail https://babel.hathitrust.org/flags/web/catalog-release-\${TODAY} --resolve "babel.hathitrust.org:443:6.5.4.3|) }
         it { is_expected.to contain_cron('catalog solr index release')
           .with(hour: 6, minute: 30)
         }
@@ -42,7 +41,8 @@ describe 'nebula::profile::hathitrust::solr6::catalog' do
         let(:params) do
           { is_primary_site: false }
         end
-        it { is_expected.to contain_file('/usr/local/bin/index-release').with_content(%r{^#if ! curl -A SOLR -s --retry 5 --fail https://babel.hathitrust.org}) }
+        it { is_expected.to contain_file('/usr/local/bin/index-release')
+          .without_content(%r|^if ! curl -A SOLR -s --retry 5 --fail https://babel.hathitrust.org|) }
         it { is_expected.to contain_cron('catalog solr index release')
           .with(hour: 6, minute: 25)
         }
--- spec/classes/profile/hathitrust/solr6/lss_spec.rb
+++ spec/classes/profile/hathitrust/solr6/lss_spec.rb
@@ -13,16 +13,14 @@ describe 'nebula::profile::hathitrust::solr6::lss' do
       it { is_expected.to contain_file('/usr/local/bin/index-release')
         .with_content(%r|BASE=/htsolr/lss|)
         .with_content(%r|^SNAP=".snapshot/htsolr-lss_\${TODAY}"$|)
-        .with_content(%r|CORES="66 99"|)
+        .with_content(/CORES="66 99"/)
         .with_content(%r|/flags/STOPLSSRELEASE|)
         .with_content(%r|/bin/echo "STOPLSSRELEASE flag present|)
-        .with_content(%r|curl -A SOLR -s --retry 5 --fail https://babel.hathitrust.org/flags/web/lss-release-|)
-        .with_content(%r{babel.hathitrust.org:443:5.4.3.2})
         .with_content(%r|SEGMENTS=`ls \${BASE}/cores/\${s}/\${SNAP}/core-\${s}x/data/index/|)
-        .without_content(%r|if \[ \${SEGMENTS} -eq 0 \];|)
-           .with_content(%r(if \[ \${SEGMENTS} -lt 1 \] \|\| \[ \${SEGMENTS} -gt 2 \];))
+        .without_content(/if \[ \${SEGMENTS} -eq 0 \];/)
+           .with_content(/if \[ \${SEGMENTS} -lt 1 \] \|\| \[ \${SEGMENTS} -gt 2 \];/)
         .with_content(%r|rm -f \${SYMLINKBASE}/lss-\${s} && ln -s \${BASE}/cores/\${s}/\${SNAP} \${SYMLINKBASE}/lss-\${s}$|)
-        .with_content(/^## run the first query to initialize catalog solr$/)
+        .without_content(/^# run the first query to initialize catalog solr$/)
         .with_content(/^# run the first query to initialize lss solr$/)
       }
       it { is_expected.to contain_cron('lss solr index release')
@@ -33,7 +31,9 @@ describe 'nebula::profile::hathitrust::solr6::lss' do
         let(:params) do
           { is_primary_site: true }
         end
-        it { is_expected.to contain_file('/usr/local/bin/index-release').with_content(%r{^if ! curl -A SOLR -s --retry 5 --fail https://babel.hathitrust.org}) }
+        it { is_expected.to contain_file('/usr/local/bin/index-release')
+          .with_content(%r|^if ! curl -A SOLR -s --retry 5 --fail https://babel.hathitrust.org/flags/web/lss-release-\${TODAY} --resolve "babel.hathitrust.org:443:5.4.3.2"|)
+        }
         it { is_expected.to contain_cron('lss solr index release')
           .with(hour: 6, minute: 0)
         }
@@ -42,7 +42,8 @@ describe 'nebula::profile::hathitrust::solr6::lss' do
         let(:params) do
           { is_primary_site: false }
         end
-        it { is_expected.to contain_file('/usr/local/bin/index-release').with_content(%r{^#if ! curl -A SOLR -s --retry 5 --fail https://babel.hathitrust.org}) }
+        it { is_expected.to contain_file('/usr/local/bin/index-release')
+          .without_content(%r|^if ! curl -A SOLR -s --retry 5 --fail https://babel.hathitrust.org|) }
         it { is_expected.to contain_cron('lss solr index release')
           .with(hour: 5, minute: 55)
         }
@@ -53,7 +54,7 @@ describe 'nebula::profile::hathitrust::solr6::lss' do
           { is_primary_node: true }
         end
         it {
-          is_expected.to contain_file('/usr/local/bin/index-release').with_content(%r{^touch /htapps/babel/flags/web/lss-release-\${TODAY}$})
+          is_expected.to contain_file('/usr/local/bin/index-release').with_content(%r|^touch /htapps/babel/flags/web/lss-release-\${TODAY}$|)
         }
       end
       context 'on non-primary node' do
@@ -61,7 +62,7 @@ describe 'nebula::profile::hathitrust::solr6::lss' do
           { is_primary_node: false }
         end
         it {
-          is_expected.to contain_file('/usr/local/bin/index-release').with_content(%r{^#touch /htapps/babel/flags/web/lss-release-\${TODAY}$})
+          is_expected.to contain_file('/usr/local/bin/index-release').without_content(%r|^touch /htapps/babel/flags/web/lss-release-\${TODAY}$|)
         }
       end
 
