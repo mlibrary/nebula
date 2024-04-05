ed2f4aa33ce5171817f6abeaee0ed8fb205cf754
Ryan Rotter <rrotter@users.noreply.github.com>
2024-04-05 15:51:31 -0400

    parameterize kubernetes apt repo url
    Kubernetes apt repo now changes with each new minor version. This lets us put
    the source url in hieradata, making these frequent changes easier.

--- manifests/profile/kubernetes/apt.pp
+++ manifests/profile/kubernetes/apt.pp
@@ -1,11 +1,24 @@
-class nebula::profile::kubernetes::apt {
+# nebula::profile::apt
+#
+# install kubernets apt source
+#
+# @example
+#   include nebula::profile::apt
+#
+#   nebula::profile::kubernetes::apt::location: "https://pkgs.k8s.io/core:/stable:/v1.29/deb/"
+class nebula::profile::kubernetes::apt (
+  String $location,
+) {
   apt::source { 'kubernetes':
-    location => 'https://pkgs.k8s.io/core:/stable:/v1.28/deb/',
+    location => $location,
     release  => '/',
     repos    => '',
+    # per https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+    # this key does not change for new releases. This will only need to change
+    # if the listed version is removed from the deb server.
     key      => {
       'id'     => 'DE15B14486CD377B9E876E1A234654DA9A296436',
-      'source' => 'https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key',
+      'source' => 'https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key',
     },
   }
 }
--- spec/classes/profile/kubernetes/kubelet_spec.rb
+++ spec/classes/profile/kubernetes/kubelet_spec.rb
@@ -76,12 +76,12 @@ describe 'nebula::profile::kubernetes::kubelet' do
 
         it do
           is_expected.to contain_apt__source('kubernetes').with(
-            location: 'https://pkgs.k8s.io/core:/stable:/v1.28/deb/',
+            location: 'https://pkgs.k8s.io/core:/stable:/v1.29/deb/',
             release: '/',
             repos: '',
             key: {
               'id'     => 'DE15B14486CD377B9E876E1A234654DA9A296436',
-              'source' => 'https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key',
+              'source' => 'https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key',
             },
           )
         end
