68395a76204a413dad869fd14f60c9130b88b784
Anthony Thomas <antmoth@umich.edu>
2024-04-30 10:13:13 -0400

    s/$name/$volume_name

--- manifests/local_storage_volume.pp
+++ manifests/local_storage_volume.pp
@@ -6,31 +6,31 @@
 # 
 # Create a volume to use for kubernetes local storage on a worker node
 ##
-# @param name The name of the volume
+# @param volume_name The (conventionally "${UUID}-pvc") name of the volume
 # @param capacity The disk capacity in MB
 
 define nebula::local_storage_volume (
-  String $name,
+  String $volume_name,
   Integer $mib_capacity
 ) {
 
-  file { "/mnt/local-pvs/mounts/$name":
+  file { "/mnt/local-pvs/mounts/$volume_name":
     ensure => 'directory'
   }
 
   exec { "make disk file":
-    command => "dd if=/dev/zero of=/mnt/local-pvs/disks/${name} bs=1048576 count=${mib_capacity}",
-    creates => "/mnt/local-pvs/disks/${name}"
+    command => "dd if=/dev/zero of=/mnt/local-pvs/disks/${volume_name} bs=1048576 count=${mib_capacity}",
+    creates => "/mnt/local-pvs/disks/${volume_name}"
   }
 
   exec { "make it a filesystem":
-    command => "mkfs.ext4 -m 0 /mnt/local-pvs/disks/${name}",
-    unless => "file /mnt/local-pvs/disks/${name} | grep ext4"
+    command => "mkfs.ext4 -m 0 /mnt/local-pvs/disks/${volume_name}",
+    unless => "file /mnt/local-pvs/disks/${volume_name} | grep ext4"
   }
 
-  mount { "/mnt/local-pvs/mounts/${name}":
+  mount { "/mnt/local-pvs/mounts/${volume_name}":
     ensure  => 'mounted',
-    device  => "/mnt/local-pvs/disks/${name}",
+    device  => "/mnt/local-pvs/disks/${volume_name}",
     options => "loop,rw,usrquote,grpquota",
     fstype  => 'ext4',
   }
