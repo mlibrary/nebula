725dc72d02f1c0ce9bd75f97497cc3dcb7da1d29
Ryan Rotter <rrotter@users.noreply.github.com>
2024-10-14 16:21:01 -0400

    remove i40e-dkms package
    This package was added to work around a buggy ethernet driver in 2018:
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=892105
    
    This has surely been patched in the last 6 years. Also, the package no
    longer works w/ current kernel/debian.

--- spec/classes/profile/base_spec.rb
+++ spec/classes/profile/base_spec.rb
@@ -154,18 +154,9 @@ describe 'nebula::profile::base' do
       end
 
       it { is_expected.not_to contain_package('i40e-dkms') }
-
       context 'with an Intel X710 network card' do
         let(:facts) do
           super().merge('network_cards' => ['Intel Corporation Ethernet Controller X710 for 10GbE SFP+ (rev 01)'])
-        end
-
-        it { is_expected.to contain_package('i40e-dkms') }
-      end
-
-      context 'without a Intel X710 network card' do
-        let(:facts) do
-          super().merge('network_cards' => ['Acme Corporation Ethernet Widget 10GbE', 'Virtual Network Adapter'])
         end
 
         it { is_expected.not_to contain_package('i40e-dkms') }
