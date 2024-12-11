ec5d611dee9411909ccab953a6f18d57e16e7248
Ryan Rotter <rrotter@users.noreply.github.com>
2024-12-11 18:53:15 -0500

    add spec_helper/stubloader
    apparantly a hack to allow rspec mocks to work

---
+++ spec/spec_helper/stub_loader.rb
@@ -0,0 +1,39 @@
+# Stub the default dependency loader to do normal resolution except where overridden
+def stub_loader!
+  allow_any_instance_of(Puppet::Pops::Loader::DependencyLoader).to receive(:load).and_call_original
+end
+
+# Stub the dependency loader to resolve a named function to a double of some type
+#
+# @param name [String] The name of the puppet function to stub
+# @param dbl [RSpec double] Optional RSpeck double with `call` mocked;
+#        will be used instead of the block if supplied
+# @yield [*args] A block that takes all parameters given to the puppet function;
+#        required if dbl is not supplied, and ignored if dbl is supplied
+def stub_function(name, dbl = nil, &func)
+  func = dbl || func
+  stub = ->(_scope, *args, &block) do
+    func.call(*args, &block)
+  end
+  allow_any_instance_of(Puppet::Pops::Loader::DependencyLoader).to receive(:load).with(:function, name).and_return(stub)
+end
+
+def stub_with_call(name)
+  double(name).tap do |dbl|
+    dbl.define_singleton_method(:allow_call) do
+      allow(dbl).to receive(:call)
+    end
+    stub_function(name, dbl)
+  end
+end
+
+def stub(name)
+  dbl = double(name)
+
+  yield dbl
+  stub_function(name, dbl)
+end
+
+def allow_call(dbl)
+  allow(dbl).to receive(:call)
+end
--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -4,3 +4,4 @@ require 'faker'
 require 'spec_helper/deep_merge'
 require 'spec_helper/mock_function'
 include RSpecPuppetUtils
+require 'spec_helper/stub_loader'
