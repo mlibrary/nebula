79e4ad2f5faae40aa2469e882ef35412e284f00b
Ryan Rotter <rrotter@users.noreply.github.com>
2024-12-17 14:27:28 -0500

    public_ip function: rm legacy fact `ipaddress`

--- spec/functions/public_ip_spec.rb
+++ spec/functions/public_ip_spec.rb
@@ -6,5 +6,11 @@
 require 'spec_helper'
 
 describe 'public_ip' do
-  it { is_expected.to run.and_return('172.16.254.254') }
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to run.and_return(facts[:networking]['ip']) }
+    end
+  end
 end
