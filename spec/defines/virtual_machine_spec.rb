d9e7670acc2987c9570f79d25c8a94fd4f1f7eb6
Ryan Rotter <rrotter@users.noreply.github.com>
2025-11-05 10:48:44 -0500

    install "puppet" from openvox repo in preseed
    Remove our repo from preseed because it was only there installing
    puppet. Replace w/ openvox, and use updated package name for install.

--- spec/defines/virtual_machine_spec.rb
+++ spec/defines/virtual_machine_spec.rb
@@ -47,7 +47,7 @@ describe "nebula::virtual_machine" do
           %r{^d-i netcfg/get_hostname string vmname\.default\.invalid$},
           %r{^d-i netcfg/get_domain string default\.invalid$},
           %r{^d-i netcfg/hostname string vmname\.default\.invalid$},
-          %r{^d-i apt-setup/local0/repository string https://apt\.lib\.umich\.edu bullseye main$.*^d-i apt-setup/local0/key string https://apt\.lib\.umich\.edu/mlibrary-archive-keyring\.asc$}m,
+          %r{^d-i apt-setup/local0/repository string https://apt\.voxpupuli\.org debian11 openvox8$.*^d-i apt-setup/local0/key string https://apt\.voxpupuli\.org/openvox-keyring\.gpg$}m,
           %r{\swget -O /target/etc/puppetlabs/puppet/puppet\.conf\s.*https://files\.default\.invalid/puppet\.conf}m
         ].each do |line|
           it { is_expected.to contain_preseed.with_content(line) }
