af4e295d9828b5973d3925d366c56a1126f593a5
Ryan Rotter <rrotter@users.noreply.github.com>
2025-11-21 15:58:06 -0500

    divert pam.d/sshd
    Since we can't replace this with a drop-in, tell dpkg to manage
    /etc/pam.d/sshd-defaults *instead* as though it were /etc/pam.d/sshd.

--- manifests/profile/networking/sshd.pp
+++ manifests/profile/networking/sshd.pp
@@ -42,13 +42,33 @@ class nebula::profile::networking::sshd (
     source => "puppet:///modules/nebula/default/${facts['os']['distro']['codename']}/etc/ssh/ssh_config",
   }
 
-  # The PAM defaults for sshd have been unchanged between jessie and buster...
-  # If we need to update them, we can add some file selection here.
-  file { '/etc/pam.d/sshd-defaults':
-    source => 'puppet:///modules/nebula/pam.d/sshd-defaults',
+  exec { 'divert pam.d/sshd':
+    creates => '/etc/pam.d/sshd-defaults',
+    timeout => 30,
+    command => '/usr/bin/dpkg-divert --rename --divert /etc/pam.d/sshd-defaults --add /etc/pam.d/sshd',
+    before  => File['/etc/pam.d/sshd-defaults'],
+  }
+  # Diverts file without moving. This is required if `sshd-defaults` already exists, but isn't safe unless
+  # we're also writing `sshd-defaults` afterwords.
+  exec { 'divert pam.d/sshd 2':
+    # TODO: remove this exec once all existing hosts have been remediated.
+    unless  => "/usr/bin/test `/usr/bin/dpkg-divert --truename /etc/pam.d/sshd` = '/etc/pam.d/sshd-defaults'",
+    timeout => 30,
+    command => '/usr/bin/dpkg-divert --no-rename --divert /etc/pam.d/sshd-defaults --add /etc/pam.d/sshd',
+    before  => File['/etc/pam.d/sshd-defaults'],
+    require => Exec['divert pam.d/sshd'],
   }
 
-  concat_file { '/etc/pam.d/sshd': }
+  # Reset /etc/pam.d/sshd-defaults to original distro config for /etc/pam.d/sshd.
+  # TODO: This should be deleted once all existing hosts have been remediated.
+  file { '/etc/pam.d/sshd-defaults':
+    source => "puppet:///modules/nebula/default/${facts['os']['distro']['codename']}/etc/pam.d/sshd",
+  }
+
+  concat_file { '/etc/pam.d/sshd':
+    require => File['/etc/pam.d/sshd-defaults'],
+    # require => Exec['divert pam.d/sshd'],
+  }
 
   concat_fragment { '/etc/pam.d/sshd: base':
     target  => '/etc/pam.d/sshd',
