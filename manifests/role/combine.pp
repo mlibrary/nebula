68a2676ea9da668548b927fcb8bc546368866407
Anthony Thomas <antmoth@umich.edu>
2026-01-19 14:38:23 -0500

    role for combine host to set up SSL

---
+++ manifests/role/combine.pp
@@ -0,0 +1,17 @@
+# Copyright (c) 2026 The Regents of the University of Michigan.
+# All Rights Reserved. Licensed according to the terms of the Revised
+# BSD License. See LICENSE.txt for details.
+
+# Role for hosting the Combine metadata application
+#
+# @example
+#   include nebula::role::combine
+class nebula::role::combine (
+) {
+  include nebula::role::umich
+  class { 'nebula::profile::https_to_port':
+    port => 80,
+  }
+
+}
+
