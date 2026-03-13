457e9269812a352a26a23c7c504358521aa4a132
Ryan Rotter <rrotter@users.noreply.github.com>
2026-03-13 16:29:31 -0400

    unattended-upgrades: rm old `ensure => absent`

--- manifests/profile/unattended_upgrades.pp
+++ manifests/profile/unattended_upgrades.pp
@@ -6,10 +6,4 @@ class nebula::profile::unattended_upgrades {
     only_on_ac_power                   => false,
     skip_updates_on_metered_connection => false,
   }
-
-  # TODO: DELETE THIS
-  # this file is no longer needed, this needs to run once on existing hosts
-  file { '/etc/apt/apt.conf.d/51unattended-upgrades-extra':
-    ensure => absent
-  }
 }
