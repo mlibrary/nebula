3a4606270b77b4a8bc186d43b26701df471f5170
Anthony Thomas <antmoth@umich.edu>
2026-05-01 01:56:19 -0400

    nginx timeouts increase for combine?

--- manifests/profile/combine.pp
+++ manifests/profile/combine.pp
@@ -4,6 +4,19 @@
 
 # Profile for a server running the Combine metadata harvester
 class nebula::profile::combine {
+  include nebula::profile::https_to_port
+
+  $letsencrypt_directory = $facts['letsencrypt_directory'][$server_name]
+
+  if $letsencrypt_directory {
+    # Only serve the HTTPS site if the cert already exists.
+    Nginx::Resource::Server <| title == 'https-forwarder' |> {
+      proxy_read_timeout    => '900s',
+      proxy_connect_timeout => '900s',
+      proxy_send_timeout    => '900s',
+    }
+  }
+
   file { '/etc/sysctl.d/combine.conf':
     content => template('nebula/profile/combine/sysctl.conf.erb'),
   }
