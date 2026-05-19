a902270e1315a79625cb5851a9e939b504649406
Ryan Rotter <rrotter@users.noreply.github.com>
2026-05-19 19:51:07 -0400

    puppet server: add g10k user
    create user with ssh keys for syncing git repos

--- manifests/profile/puppet/server.pp
+++ manifests/profile/puppet/server.pp
@@ -2,8 +2,29 @@
 #
 # @example
 #   include nebula::profile::puppet::server
-class nebula::profile::puppet::server () {
+class nebula::profile::puppet::server (
+  Array[String] $deploy_keys,
+) {
   ensure_packages([
     'g10k',
   ])
+
+  $g10k_user = 'g10k'
+  $g10k_home = "/var/lib/${g10k_user}"
+  user { 'g10k':
+    ensure     => present,
+    comment    => 'g10k daemon user',
+    home       => $g10k_home,
+    managehome => true,
+    system     => true,
+    shell      => '/bin/bash',
+  }
+  $deploy_keys.each |$repo| {
+    ssh_keygen { $repo:
+      type     => 'ed25519',
+      user     => $g10k_user,
+      require  => User[$g10k_user],
+      filename => "${g10k_home}/.ssh/${repo}",
+    }
+  }
 }
