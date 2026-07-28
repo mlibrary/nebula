14bc0a59ea12f94e1aea2cf09cb774603d2c5364
Ryan Rotter <rrotter@users.noreply.github.com>
2026-07-28 15:34:00 -0400

    mariadb backup_db cronjob
    - enable by setting $backup_cron_weekday
    - filled in docs for profile::mariadb::server
    - backup_db script
      - based off our old mysql backup scripts
      - we only keep one version of backup at a time, we'll use nfs snaps to
        manage additional versions
      - inbuilt support for mariadb-dump --ignore-table, --ignore-table-data
      - use `zstd` (in place of `pigz`)
      - don't use `split`: 50GiB files are managable, and most will be far
        smaller
      - backup_db cron is uninstalled when not requested in heira
      - backup_db script is uninsalled when nfs mount not configured
    - zstd uses default compression level; this could be tweaked later
    - uses cron::job (from voxpupuli) not cron (aka puppetlabs/cron_core)
      - cron is in /etc/cron.d, which may be surprising

--- manifests/profile/mariadb/server.pp
+++ manifests/profile/mariadb/server.pp
@@ -1,9 +1,28 @@
-# install and configure mariadb-server, using mariadb apt repo
+# nebula::profile::mariadb::server
+#
+# Manage mariadb-server
+#
+# @summary Install and configure mariadb-server, using mariadb apt repo
+#
+# @param backup_path Mount point for optional NFS mount for mariadb backups (default: "/mnt/backup")
+# @param backup_nfs_target NFS mount target for mariadb backups. Set this to enable mounting. (example: "nfs.example.com/path/to/export")
+# @param backup_nfs_options NFS mount options (default: "auto,hard")
+# @param backup_cron_weekday Set this to enable backup cron. See puppet/cron docs for syntax. (examples: "*", "1,3,5", "1-5")
+# @param backup_ignore_table Table(s) to ignore when running mariadb-dump
+# @param backup_ignore_table_data Table(s) to only backup structure when running mariadb-dump
+# @param extra_conf Arbitrary mariadb conf arguments. Expected to be a multi line string. (example: "log_bin = /var/log/mysql/mysql-bin.log\nserver_id = 123")
+#
+# @example
+#   include nebula::profile::mariadb::server
+
 class nebula::profile::mariadb::server (
-  String $backup_path = '/mnt/backup',
-  Optional[String] $backup_nfs_target = undef,
-  String $backup_nfs_options = 'auto,hard',
-  Optional[String] $extra_conf = undef,
+  String                                      $backup_path              = '/mnt/backup',
+  Optional[String]                            $backup_nfs_target        = undef,
+  String                                      $backup_nfs_options       = 'auto,hard',
+  Optional[Cron::Weekday]                     $backup_cron_weekday      = undef, # set to enable backup cron
+  Optional[Variant[String, Array[String[1]]]] $backup_ignore_table      = undef,
+  Optional[Variant[String, Array[String[1]]]] $backup_ignore_table_data = undef,
+  Optional[String]                            $extra_conf               = undef,
 ) {
   @nebula::taghosts::tag { 'mariadb': }
 
@@ -37,7 +56,10 @@ class nebula::profile::mariadb::server (
   }
 
   if $backup_nfs_target {
-    ensure_packages(['nfs-common'])
+    ensure_packages([
+      'nfs-common',
+      'zstd',
+    ])
 
     file { $backup_path: ensure => directory }
 
@@ -47,6 +69,56 @@ class nebula::profile::mariadb::server (
       options => $backup_nfs_options,
       fstype  => 'nfs',
       require => [Package['nfs-common'], File[$backup_path]],
+    }
+
+    $_backup_ignore_table = $backup_ignore_table ? {
+      undef   => [],
+      String  => [$backup_ignore_table],
+      default => $backup_ignore_table
+    }
+    $_backup_ignore_table.each |String $t| {
+      if $t !~ /^[A-Za-z0-9_]+\.[A-Za-z0-9_]+$/ {
+        fail("nebula::profile::mariadb::server: backup_ignore_table entry '${t}' must be a valid MariaDB table name (e.g. 'my_db.my_table')")
+      }
+    }
+    $_backup_ignore_table_data = $backup_ignore_table_data ? {
+      undef   => [],
+      String  => [$backup_ignore_table_data],
+      default => $backup_ignore_table_data
+    }
+    $_backup_ignore_table_data.each |String $t| {
+      if $t !~ /^[A-Za-z0-9_]+\.[A-Za-z0-9_]+$/ {
+        fail("nebula::profile::mariadb::server: backup_ignore_table_data entry '${t}' must be a valid MariaDB table name (e.g. 'my_db.my_table')")
+      }
+    }
+
+    file { '/usr/local/bin/backup_db':
+      content => template('nebula/mariadb/backup_db.erb'),
+      mode    => '0755',
+      require => Package['zstd'],
+    }
+
+    if $backup_cron_weekday {
+      cron::job { 'backup_db':
+        command     => '/usr/local/bin/backup_db',
+        minute      => fqdn_rand(60,'backup_db minute'),
+        hour        => fqdn_rand(3,'backup_db hour'),
+        weekday     => $backup_cron_weekday,
+        environment => [
+          'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"',
+        ],
+        description => "nebula::profile::mariadb::server: backup mariadb to ${backup_path}",
+      }
+    } else {
+      cron::job { 'backup_db': ensure => absent }
+    }
+  } else {
+    # we don't want this script to exist if we don't have an nfs mount to write to
+    file { '/usr/local/bin/backup_db': ensure => absent }
+    cron::job { 'backup_db': ensure => absent }
+
+    if $backup_cron_weekday {
+      fail('backup_cron_weekday may not be set without setting backup_nfs_target')
     }
   }
 }
--- spec/classes/profile/mariadb/server_spec.rb
+++ spec/classes/profile/mariadb/server_spec.rb
@@ -34,12 +34,21 @@ describe "nebula::profile::mariadb::server" do
 
       it "no default nfs backup" do
         is_expected.not_to contain_mount("/mnt/backup")
+        is_expected.to contain_cron__job("backup_db").with_ensure("absent")
+        is_expected.not_to contain_package("nfs-common")
       end
-      it { is_expected.not_to contain_package("nfs-common") }
 
-      context "with backup_nfs_target set" do
+      it "removes backup_db when nfs mount not configured" do
+        is_expected.to contain_file("/usr/local/bin/backup_db").with_ensure("absent")
+      end
+
+      context "with backup_nfs_target, backup_ignore_table* set" do
         let(:params) do
-          {backup_nfs_target: "example.invalid:/some/nfs/path"}
+          {
+            backup_nfs_target: "example.invalid:/some/nfs/path",
+            backup_ignore_table: ["ephemeral.table", "some_db.just_cache_here"],
+            backup_ignore_table_data: "important_db.with_unimportant_data"
+          }
         end
 
         it "creates backup dir" do
@@ -53,6 +62,75 @@ describe "nebula::profile::mariadb::server" do
         end
 
         it { is_expected.to contain_package("nfs-common") }
+        it { is_expected.to contain_package("zstd") }
+
+        it "installs backup_db script, w/ --ignore-table flags" do
+          is_expected.to contain_file("/usr/local/bin/backup_db")
+            .with_content(%r{^BACKUP_DIR='/mnt/backup/foo'$})
+            .with_content(/--ignore-table=some_db.just_cache_here/)
+            .with_content(/--ignore-table-data=important_db.with_unimportant_data/)
+        end
+        it "deletes backup_db cron" do
+          is_expected.to contain_cron__job("backup_db").with_ensure("absent")
+        end
+      end
+
+      context "with backup_nfs_target, backup_cron_weekday, and backup_path set" do
+        let(:params) do
+          {
+            backup_nfs_target: "example.invalid:/some/nfs/path",
+            backup_cron_weekday: "1,3,5",
+            backup_path: "/some/random/path"
+          }
+        end
+
+        it "installs backup_db script, w/o --ignore-table flags" do
+          is_expected.to contain_file("/usr/local/bin/backup_db")
+            .with_content(%r{^BACKUP_DIR='/some/random/path/foo'$})
+            .with_content(/--user=root \\\n  \| zstd/)
+        end
+        it "creates backup_db cron job" do
+          is_expected.to contain_cron__job("backup_db")
+            .with_command("/usr/local/bin/backup_db")
+            .with_description(%r{backup mariadb to /some/random/path$})
+        end
+        it { is_expected.to contain_mount("/some/random/path") }
+      end
+
+      [
+        [" foo.bar", "some_db.just_cache_here"],
+        ["foo.bar", "not.areal.table"],
+        ["foo.bar", ""],
+        ""
+      ].each do |tables|
+        [:backup_ignore_table, :backup_ignore_table_data].each do |p|
+          context "invalid #{p}: '#{tables}'" do
+            let(:params) do
+              {:backup_nfs_target => "example.invalid:/some/nfs/path", p => tables}
+            end
+            it { is_expected.not_to compile }
+          end
+        end
+      end
+
+      context "valid backup_ignore_table: 'some.table'" do
+        let(:params) do
+          {backup_nfs_target: "example.invalid:/some/nfs/path", backup_ignore_table: "some.table"}
+        end
+        it { is_expected.to contain_file("/usr/local/bin/backup_db").with_content(/--ignore-table=some.table/).without_content(/--ignore-table-data=some.table/) }
+      end
+      context "valid backup_ignore_table_data: 'some.table'" do
+        let(:params) do
+          {backup_nfs_target: "example.invalid:/some/nfs/path", backup_ignore_table_data: "some.table"}
+        end
+        it { is_expected.to contain_file("/usr/local/bin/backup_db").without_content(/--ignore-table=some.table/).with_content(/--ignore-table-data=some.table/) }
+      end
+
+      context "with backup_cron_weekday set (but no backup_nfs_target)" do
+        let(:params) do
+          {backup_cron_weekday: "1,3,5"}
+        end
+        it { is_expected.not_to compile }
       end
     end
   end
