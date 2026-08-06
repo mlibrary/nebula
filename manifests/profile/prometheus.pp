ab3755f820fb416b4d2a4213310af1b841077a90
Matthew Alexander LaChance <mattlach@umich.edu>
2026-08-06 14:27:20 -0400

    Divert packaged prometheus configs

--- manifests/profile/prometheus.pp
+++ manifests/profile/prometheus.pp
@@ -11,8 +11,34 @@ class nebula::profile::prometheus (
   package { 'prometheus-pushgateway': }
   service { 'prometheus-pushgateway': }
 
+  exec { 'divert /etc/prometheus/prometheus.yml':
+    creates => '/etc/prometheus/prometheus.yml.dist',
+    command => '/usr/bin/dpkg-divert --rename --divert /etc/prometheus/prometheus.yml.dist --add /etc/prometheus/prometheus.yml',
+    require => Package['prometheus'],
+  }
+
+  exec { 'divert /etc/default/prometheus-pushgateway':
+    creates => '/etc/default/prometheus-pushgateway.dist',
+    command => '/usr/bin/dpkg-divert --rename --divert /etc/default/prometheus-pushgateway.dist --add /etc/default/prometheus-pushgateway',
+    require => Package['prometheus-pushgateway'],
+  }
+
   file { '/etc/prometheus/prometheus.yml':
     content => template('nebula/profile/prometheus/config.yml.erb'),
     notify  => Service['prometheus'],
+    require => Exec['divert /etc/prometheus/prometheus.yml'],
+  }
+
+  file { '/var/lib/prometheus/pushgateway':
+    ensure => 'directory',
+  }
+
+  file { '/etc/default/prometheus-pushgateway':
+    content => "ARGS=\"--persistence.file=/var/lib/prometheus/pushgateway/archive\"\n",
+    notify  => Service['prometheus-pushgateway'],
+    require => [
+      Exec['divert /etc/default/prometheus-pushgateway'],
+      File['/var/lib/prometheus/pushgateway'],
+    ]
   }
 }
