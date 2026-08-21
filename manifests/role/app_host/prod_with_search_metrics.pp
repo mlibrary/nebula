a23f348b4df713ca48d0e9112fdcd42862caf051
Jody Nega <60393585+umjnega@users.noreply.github.com>
2026-08-21 10:34:18 -0400

    Disable solr_monitor in app_host role
    Commented out the inclusion of solr_monitor profile due to dependency on solr Profile.

--- manifests/role/app_host/prod_with_search_metrics.pp
+++ manifests/role/app_host/prod_with_search_metrics.pp
@@ -5,5 +5,6 @@
 class nebula::role::app_host::prod_with_search_metrics {
   include nebula::role::app_host::prod
   include nebula::profile::client_cert_https
-  include nebula::profile::search::catalog::solr_monitor
+  # solr_monitor can't work without solr Profile
+  #include nebula::profile::search::catalog::solr_monitor
 }
